1135cd1f07a6f959648d37e24ad57e4f2aad4f50
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-02-27 16:04:16 +0900

    Create TwoSat.cpp

---
+++ graph/TwoSat.cpp
@@ -0,0 +1,39 @@
+struct TwoSat{
+  int n;
+  SCC scc;
+  inline int rev(int x){
+    if(x >= n) return x - n;
+    else return x + n;
+  }
+
+  TwoSat(int n):n(n), scc(n*2){}
+
+  void add_or(int u, int v, bool f=true, bool g=true){
+    if(!f) u = rev(u);
+    if(!g) v = rev(v);
+    scc.addEdge(rev(u), v);
+    scc.addEdge(rev(v), u);
+  }
+
+  void add_nand(int u, int v, bool f=true, bool g=true){
+    if(!f) u = rev(u);
+    if(!g) v = rev(v);
+    scc.addEdge(u, rev(v));
+    scc.addEdge(v, rev(u));
+  }
+
+  void add_one(int u, bool f=true){
+    if(!f) u = rev(u);
+    scc.addEdge(rev(u),u);
+  }
+
+  VI answer(){
+    scc.build();
+    VI res(n);
+    REP(i,n){
+      if(scc.issame(i,rev(i))) return {};
+      res.at(i) = scc.at(i) > scc.at(rev(i));
+    }
+    return res;
+  }
+};
