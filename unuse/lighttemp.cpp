0cdd98e8780a555ef9d588ead1e748cad4d18476
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-02-27 15:37:51 +0900

    Create lighttemp.cpp

---
+++ unuse/lighttemp.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using pii = pair<int, int>;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
