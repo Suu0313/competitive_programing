c2367d693f236391ca7d237cd2b327aba503c874
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-03-05 22:17:54 +0900

    Update TopoSort.cpp
    expanded macros

--- graph/TopoSort.cpp
+++ graph/TopoSort.cpp
@@ -1,22 +1,23 @@
-VI TopoSort(VVI &G){
-  VI ans;
-  int n = SZ(G);
-  VI ind(n,0);
-  REP(i,n){
+template<typename T>
+vector<int> TopoSort(const vector<vector<T>> &G){
+  vector<int> ans;
+  int n = G.size();
+  vector<int> ind(n,0);
+  for(int i = 0; i < n; i++){
     for(auto e : G.at(i)){
       ind.at(e)++;
     }
   }
   queue<int> que;
-  REP(i,n){
+  for(int i = 0; i < n; i++){
     if(ind.at(i)==0) que.push(i);
   }
   while(!que.empty()){
     int now = que.front(); que.pop();
-    ans.PB(now);
+    ans.emplace_back(now);
     for(auto e : G.at(now)){
       ind.at(e)--;
-      if(ind.at(e)==0) que.push(e);
+      if(ind.at(e)==0) que.emplace(e);
     }
   }
   return ans;
