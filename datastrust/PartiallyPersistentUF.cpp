f305f8c71abb0906264af9925189a73b714c547c
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-03-07 14:51:39 +0900

    Update PartiallyPersistentUF.cpp
    expanded macros

--- datastrust/PartiallyPersistentUF.cpp
+++ datastrust/PartiallyPersistentUF.cpp
@@ -1,20 +1,21 @@
 struct PartiallyPersistentUF {
   vector<int> par, siz, last;
   int now;
-  vector<vector<pair<int,int>>> hist;
+  vector<vector<pair<int, int>>> hist;
   
   PartiallyPersistentUF() {}
-  PartiallyPersistentUF(int n): par(n,-1),siz(n,1),last(n,INF),now(0) {
-    iota(ALL(par),0);
-    hist.assign(n,vector<pair<int,int>>(1,{0,1}));
+  PartiallyPersistentUF(int n)
+  : par(n,-1),siz(n,1),last(n,numeric_limits<int>::max()/2),now(0) {
+    iota(par.begin(), par.end(), 0);
+    hist.assign(n,vector<pair<int, int>>(1,{0,1}));
   }
   void init(int n){
     par.resize(n);
-    iota(ALL(par),0);
+    iota(par.begin(), par.end(),0);
     siz.assign(n,1);
-    last.assign(n,INF);
+    last.assign(n,numeric_limits<int>::max()/2);
     now = 0;
-    hist.assign(n,VPII(1,{0,1}));
+    hist.assign(n,vector<pair<int, int>>(1,{0,1}));
   }
 
   int find(int t,int x) {
@@ -34,7 +35,7 @@ struct PartiallyPersistentUF {
     siz[x] += siz[y];
     par[y] = x;
     last[y] = now;
-    hist.at(x).EB(now, siz[x]);
+    hist.at(x).emplace_back(now, siz[x]);
     return {true, now-1};
   }
 
@@ -45,8 +46,8 @@ struct PartiallyPersistentUF {
   int size(int t, int x){
     x = find(t, x);
     return prev(partition_point(
-      ALL(hist.at(x)),
-      [t](PII &h){ return h.FI<=t;})
-      )->SE;
+      hist[x].begin(), hist[x].end(),
+      [t](pair<int, int> &h){ return h.first<=t;})
+      )->second;
   }
 };
