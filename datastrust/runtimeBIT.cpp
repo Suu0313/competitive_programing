549d6e0c53e4ebb3e5386ac6b380b632ad8f2a5c
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-03-07 15:00:54 +0900

    Update runtimeBIT.cpp
    change LL to U

--- datastrust/runtimeBIT.cpp
+++ datastrust/runtimeBIT.cpp
@@ -1,32 +1,32 @@
-template <typename T=int>
+template <typename T=int, typename U = long long>
 struct runtimeBIT{
-  LL n;
-  umap<LL, T> node; //1-indexed
+  U n;
+  umap<U, T> node; //1-indexed
   runtimeBIT() : n(0) {}
-  runtimeBIT(LL n_) : n(n_+1) {}
+  runtimeBIT(U n_) : n(n_+1) {}
 
-  void add(LL i, T x){
+  void add(U i, T x){
     if(i==0)return;
     for(LL idx = i; idx < n; idx += (idx & -idx)){
       node[idx] += x;
     }
   }
-  T sum(LL i){ //[1,i]
+  T sum(U i){ //[1,i]
     T s(0);
-    for(LL idx = i; idx > 0; idx -= (idx & -idx)){
+    for(U idx = i; idx > 0; idx -= (idx & -idx)){
       s += node[idx];
     }
     return s;
   }
-  T query(LL l, LL r){ //[l,r)
+  T query(U l, U r){ //[l,r)
     return sum(r-1)-sum(l-1); 
   }
 
-  LL lower_bound(T w){
+  U lower_bound(T w){
     if(w <= 0) return 0;
-    LL x = 0, r = 1;
+    U x = 0, r = 1;
     while(r < n) r *= 2;
-    for(LL len = r; len>0; len>>=1){
+    for(U len = r; len>0; len>>=1){
       if(x+len<n && node[x+len] < w){
         w -= node[x+len];
         x += len;
