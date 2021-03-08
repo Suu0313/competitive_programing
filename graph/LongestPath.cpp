08fdd1c7e4afd9c9d6fc586ef259743acd80bd9b
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-03-08 13:02:46 +0900

    Update LongestPath.cpp

--- graph/LongestPath.cpp
+++ graph/LongestPath.cpp
@@ -1,8 +1,9 @@
-int LestP(VVI &G, int i, VI &dp){
+template<typename T>
+int LestP(vector<vector<T>> &G, int i, vector<int> &dp){
   if(dp.at(i) >= 0) return dp.at(i);
   int res = 0;
   for(auto e : G.at(i)){
-    chmax(res,LestP(G,e,dp)+1);
+    res = max(res,LestP(G,e,dp)+1);
   }
   return dp.at(i) = res;
 }
