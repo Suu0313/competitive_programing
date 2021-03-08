e094a0b87afd6e9e31afad958027be7c0a6c416b
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-03-08 16:17:16 +0900

    Update ChineseRem.cpp
    Expanded macros

--- math/ChineseRem.cpp
+++ math/ChineseRem.cpp
@@ -1,19 +1,19 @@
-PLL InvGcd(LL a, LL b){
+pair<long long, long long> InvGcd(long long a, long long b){
   if(a==0) return {b, 0};
-  LL s = b, t = a;
-  LL m0 = 0, m1 = 1;
+  long long s = b, t = a;
+  long long m0 = 0, m1 = 1;
   while(t){
-    LL u = s/t; s -= t*u; m0 -= m1*u;
-    LL tmp = s; s = t; t = tmp; tmp = m0; m0 = m1; m1 = tmp;
+    long long u = s/t; s -= t*u; m0 -= m1*u;
+    long long tmp = s; s = t; t = tmp; tmp = m0; m0 = m1; m1 = tmp;
   }
   if(m0<0) m0 += b/s;
   return {s, m0};
 }
-PLL ChineseRem(const VLL &r, const VLL &m){
-  int n = SZ(r);
-  LL r0 = 0, m0 = 1;
-  REP(i,n){
-    LL r1 = r.at(i), m1 = m.at(i);
+pair<long long, long long> ChineseRem(const vector<long long> &r, const vector<long long> &m){
+  int n = r.size();
+  long long r0 = 0, m0 = 1;
+  for(int i = 0; i < n; i++){
+    long long r1 = r.at(i), m1 = m.at(i);
     r1 %= m1;
     if(r1<0) r1 += m1;
     if(m0<m1){
@@ -24,11 +24,11 @@ PLL ChineseRem(const VLL &r, const VLL &m){
       if(r0%m1 != r1) return {0,0};
       continue;
     }
-    LL g, im;
+    long long g, im;
     tie(g,im) = InvGcd(m0, m1);
-    LL u1 = m1 / g;
+    long long u1 = m1 / g;
     if((r1-r0) % g) return {0,0};
-    LL x = (r1-r0) / g % u1 * im % u1;
+    long long x = (r1-r0) / g % u1 * im % u1;
     r0 += x * m0;
     m0 *= u1;
     if(r0 < 0) r0 += m0;
