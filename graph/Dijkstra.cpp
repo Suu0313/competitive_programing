095f5fdd9d3c7e6f85017dac1876b38413cefad6
Suu0313 <shuya_0313@docomo.ne.jp>
2021-03-13 15:17:26 +0900

    add ijk

---
+++ graph/Dijkstra.cpp
@@ -0,0 +1,19 @@
+template<typename T>
+vector<T> Dijkstra(const vector<vector<edge<T>>> &g, int s){
+  priority_queue<pair<T, int>,vector<pair<T, int>>, greater<>> pq;
+  vector<T> dist(g.size(), -1);
+  vector<bool> seen(g.size(), false);
+  pq.emplace(0, s); dist[s] = 0;
+  while(!pq.empty()){
+    auto[d,v] = pq.top(); pq.pop();
+    if(seen[v]) continue;
+    seen[v] = true;
+    for(auto&&e : g[v]){
+      if(dist[e.to]==-1 || dist[e.to]>d+e.cost){
+        dist[e.to] = d + e.cost;
+        pq.emplace(dist[e.to], e.to);
+      }
+    }
+  }
+  return dist;
+}
