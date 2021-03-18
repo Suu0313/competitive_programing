904d8cc31e0833af19da4a6544f1dbf56fad05d8
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-03-18 23:04:50 +0900

    rename 2dsparcetable

--- datastrust/DisjointSparceTable2D.cpp
+++ datastrust/DisjointSparceTable2D.cpp
@@ -1,11 +1,11 @@
 template<typename T>
-struct DisjointSpaceTable2D{
+struct DisjointSparceTable2D{
   using F = function<T(T, T)>;
   const F f;
   vector<vector<vector<vector<T>>>> table;
   vector<int> lookup;
 
-  DisjointSpaceTable2D(const vector<vector<T>> &v, const F &f): f(f){
+  DisjointSparceTable2D(const vector<vector<T>> &v, const F &f): f(f){
     const int n = v.size(), m = v[0].size();
     const int nb = 32 - __builtin_clz(n), mb = 32 - __builtin_clz(m);
     table.assign(nb, vector(n, vector(mb, vector<T>(m))));
