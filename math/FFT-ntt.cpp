c6d70f75eabfc2f34fd797315302c7a0f4676f19
Suu0313 <shuya_0313@docomo.ne.jp>
2021-03-24 15:15:59 +0900

    create FFT by NTT

---
+++ math/FFT-ntt.cpp
@@ -0,0 +1,45 @@
+namespace FFT{
+  template<int mod>
+  vector<ModInt<mod>> tovMint(const vector<long long> &a){
+    vector<ModInt<mod>> res(a.size());
+    for (size_t i = 0; i < a.size(); i++) res[i] = a[i];
+    return res;
+  }
+
+  vector<long long> multiply(const vector<long long> &a, const vector<long long> &b){
+    int n = int(a.size()), m = int(b.size());
+    if (!n || !m) return {};
+
+    static constexpr unsigned long long MOD1 = 754974721;
+    static constexpr unsigned long long MOD2 = 167772161;
+    static constexpr unsigned long long MOD3 = 469762049;
+    static constexpr unsigned long long M2M3 = MOD2 * MOD3;
+    static constexpr unsigned long long M1M3 = MOD1 * MOD3;
+    static constexpr unsigned long long M1M2 = MOD1 * MOD2;
+    static constexpr unsigned long long M1M2M3 = MOD1 * MOD2 * MOD3;
+
+    static constexpr unsigned long long i1 = 190329765;
+    static constexpr unsigned long long i2 = 58587104;
+    static constexpr unsigned long long i3 = 187290749;
+
+    auto c1 = NTT::multiply(tovMint<MOD1>(a), tovMint<MOD1>(b));
+    auto c2 = NTT::multiply(tovMint<MOD2>(a), tovMint<MOD2>(b));
+    auto c3 = NTT::multiply(tovMint<MOD3>(a), tovMint<MOD3>(b));
+
+    
+    
+    vector<long long> c(n + m - 1);
+
+    for(int i = 0; i < n+m-1; i++){
+      unsigned long long x = 0, x1 = c1[i].x, x2 = c2[i].x, x3 = c3[i].x;
+        x += (x1 * i1) % MOD1 * M2M3;
+        x += (x2 * i2) % MOD2 * M1M3;
+        x += (x3 * i3) % MOD3 * M1M2;
+        int diff = (c1[i] - (long long)x).x;
+        static constexpr unsigned long long offset[5] = {0, 0, M1M2M3, 2 * M1M2M3, 3 * M1M2M3};
+        x -= offset[diff % 5];
+        c[i] = x;
+    }
+    return c;
+  }
+}
