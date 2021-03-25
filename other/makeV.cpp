f06481009b16ffda6b4436f6d9496fc8f123d829
Suu0313 <shuya_0313@docomo.ne.jp>
2021-03-25 21:20:27 +0900

    add makevalarry

--- other/makeV.cpp
+++ other/makeV.cpp
@@ -5,3 +5,8 @@ template<typename... Ts>
 auto make_v(size_t a,Ts... ts){
   return vector<decltype(make_v(ts...))>(a,make_v(ts...));
 }
+
+template<typename T>
+valarray<T> make_v(size_t a,T b){return valarray(b,a);}
+template<typename... Ts>
+auto make_v(size_t a,Ts... ts){ return valarray(make_v(ts...), a); }
