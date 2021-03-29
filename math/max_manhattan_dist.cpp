08b58109ddb52d9ef2f42a124e919cb320df10a2
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-03-29 14:02:40 +0900

    expanded macros

--- math/max_manhattan_dist.cpp
+++ math/max_manhattan_dist.cpp
@@ -1,14 +1,14 @@
 template<typename T> // n個,k次元の点
 T max_manhattan_distance(vector<vector<T>> &v){
-  int n = SZ(v);
-  int k = SZ(v.at(0));
+  int n = v.size();
+  int k = v[0].size();
   T res = 0;
   vector<T> f(n);
 
-  BREP(bit, k-1){  
-    REP(j,n){
+  for(int bit = 0; bit < (1 << (k-1)); bit++){
+    for(int j = 0; j < n; j++){
       f.at(j) = v.at(j).at(0);
-      REP(i,k-1){
+      for(int i = 0; i < k-1; i++){
         if((bit>>i)&1){
           f.at(j) += v.at(j).at(i+1);
         }else{
@@ -16,7 +16,7 @@ T max_manhattan_distance(vector<vector<T>> &v){
         }
       }
     }
-    chmax(res, Vmax(f)-Vmin(f));
+    res = max(res, max(f) - min(f));
   }
   return res;
 }
