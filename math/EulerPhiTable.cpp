ea6443cb4285e81224364d288647efbbc418425b
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-03-31 03:46:04 +0900

    rename and expanded macros

--- math/EulerPhiTable.cpp
+++ math/EulerPhiTable.cpp
@@ -1,9 +1,8 @@
-VI euler_phi_table(int n) {
-  VI euler(n+1);
-  CREP(i,n) {
-    euler[i] = i;
-  }
-  CFOR(i,2,n) {
+vector<int> euler_phi_table(int n) {
+  vector<int> euler(n+1);
+  iota(euler.begin(), euler.end(), 0);
+  
+  for(int i = 2; i <= n; i++){
     if(euler[i] == i) {
       for(int j = i; j <= n; j += i) {
         euler[j] = euler[j] / i * (i - 1);
