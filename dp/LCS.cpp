417b22609f37458dbe44bab7c18d251bff406c41
Suu0313 <shuya_0313@docomo.ne.jp>
2021-04-02 00:50:02 +0900

    bug fixed

--- dp/LCS.cpp
+++ dp/LCS.cpp
@@ -2,7 +2,7 @@ template <typename T>
 T LCS(T s, T t){
   int n = s.size(), m = t.size();
   vector dp(n+1, vector(m+1,0));
-  for(int i = 0; i < n)for(int j = 0; j < m; j++){
+  for(int i = 0; i < n; i++)for(int j = 0; j < m; j++){
     if(s.at(i)==t.at(j)){
       dp.at(i+1).at(j+1)=dp.at(i).at(j)+1;
     }else{
