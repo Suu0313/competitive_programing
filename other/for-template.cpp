ddc41f166bb12fa9167548a4488b84b5b96752d6
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-04-02 03:00:25 +0900

    bug fixed

--- other/for-template.cpp
+++ other/for-template.cpp
@@ -33,16 +33,16 @@ struct combination{
 
 struct permutation{
   struct itr{
-    vector<int> idx;
-    bool operator!=(monostate e){ return next_permutation(idx.begin(), idx.end()); }
-    void operator++() {}
+    vector<int> idx; bool fl;
+    bool operator!=(monostate e){ return fl; }
+    void operator++() { fl = next_permutation(idx.begin(), idx.end()); }
     vector<int> operator*() { return idx; }
   };
 
   int n;
   permutation(int n): n(n) {}
 
-  itr begin(){ vector<int> idx(n); iota(idx.begin(), idx.end(), 0); return {idx}; }
+  itr begin(){ vector<int> idx(n); iota(idx.begin(), idx.end(), 0); return {idx, true}; }
   monostate end() { return {}; }
 };
 
