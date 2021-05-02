2908c6163238576a0e757127df077d4bda0e92f9
Suu0313 <shuya_0313@docomo.ne.jp>
2021-05-02 22:58:26 +0900

    add polar

--- math/geometry/Geometry.cpp
+++ math/geometry/Geometry.cpp
@@ -56,6 +56,17 @@ template<class T> struct Point{
   }
 };
 
+template<typename T> Point<T> Polar(const T& rho, const T& theta = 0){
+  return Point<T>(rho * cos(theta), rho * sin(theta));
+}
+
+template<typename T = double>
+tuple<Point<T>,Point<T>,Point<T>> ClockHands(const T& h, const T& m = 0, const T& s = 0){
+  const double PI  = 3.141592653589793238462643383279;
+  double ts = 2*PI / 60 * s, tm = 2*PI / 60 * m + ts / 60 ,th = 2*PI / 12 * h + tm / 12;
+  return make_tuple(Polar<T>(1, th), Polar<T>(1, tm), Polar<T>(1, ts));
+}
+
 template<typename T> using Polygon = vector<Point<T>>;
 
 template<class T> T Cross(const Point<T> &a, const Point<T> &b) { return a.x * b.y - a.y * b.x; }
