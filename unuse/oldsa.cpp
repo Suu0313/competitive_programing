bcc72770590a5762d697ddec143bc793bdde601b
Suu0313 <shuya_0313@docomo.ne.jp>
2021-05-10 01:46:02 +0900

    update sa

--- unuse/oldsa.cpp
+++ unuse/oldsa.cpp
@@ -1,53 +1,40 @@
-struct SuffixArray{
+struct SuffixArray{ // O(Nlog^2N)
   vector<int> sa;
   const string s;
 
   SuffixArray(const string &s): sa(s.size()), s(s) {
     iota(sa.begin(), sa.end(), 0);
-    sort(sa.begin(), sa.end(), [&](int i, int j){ return s[i] < s[j]; });
 
-    int n = s.size(), k = 0;
-    vector<int> rank(n), tmp(n), nsa(n);
+    int n = s.size();
+    vector<int> rank(n), tmp(n);
+    for(int i = 0; i < n; i++) rank[i] = s[i];
 
-    tmp[sa[0]] = 0;
-    for(int i = 0; i < n-1; i++) tmp[sa[i+1]] = tmp[sa[i]] + (s[sa[i]] < s[sa[i+1]]);
-    swap(rank, tmp);
-
-    auto nr = [&](int i){ return (i+k < n) ? rank[i+k] : -1; };
-
+    int k = 1;
     auto cmp = [&](int i, int j){
       if(rank[i] != rank[j]) return rank[i] < rank[j];
-      return nr(i) < nr(j);
-    };
-    
-    auto binsort2 = [&](){
-      vector<int> cnt(n+1);
-      for(int i = 0; i < n; i++) cnt[nr(i)+1]++;
-      for(int i = 0; i < n; i++) cnt[i+1] += cnt[i];
-      for(int i = 0; i < n; i++) nsa[--cnt[nr(i)+1]] = i;
+      int ni = (i+k < n) ? rank[i+k] : -1;
+      int nj = (j+k < n) ? rank[j+k] : -1;
+      return ni < nj;
     };
 
-    auto binsort = [&](){
-      binsort2();
-      vector<int> cnt(n);
-      for(int i = 0; i < n; i++) cnt[rank[i]]++;
-      for(int i = 0; i < n-1; i++) cnt[i+1] += cnt[i];
-      for(int i = n-1; i >= 0; i--) sa[--cnt[rank[nsa[i]]]] = nsa[i];
-    };
-
-    for(k = 1; k < n; k <<= 1){
-      binsort(); tmp[sa[0]] = 0;
+    for(; k < n; k <<= 1){
+      sort(sa.begin(), sa.end(), cmp);
+      tmp[sa[0]] = 0;
       for(int i = 0; i < n-1; i++) tmp[sa[i+1]] = tmp[sa[i]] + cmp(sa[i], sa[i+1]);
-      swap(rank, tmp);
+      for(int i = 0; i < n; i++) rank[i] = tmp[i];
     }
   }
 
   int operator[](int k) const { return sa[k]; }
   size_t size() const { return s.size(); }
 
-  int cmp_substr(const string &t, int ps = 0) const {
-    auto ls = s.substr(ps, t.size());
-    return ls<t ? -1 : (ls==t ? 0 : 1);
+  int cmp_substr(const string &t, int ps = 0, int pt = 0) const {
+    int n = s.size(), m = t.size();
+    for(; ps < n && pt < m; ps++, pt++){
+      if(s[ps] < t[pt]) return -1;
+      if(s[ps] > t[pt]) return 1;
+    }
+    return pt < m ? -1 : 0;
   }
 
   int lower_bound(const string &t) const {
@@ -80,7 +67,7 @@ struct SuffixArray{
   void dump() const {
     int n = s.size();
     for(int i = 0; i < n; i++){
-      cout << i << ": " << s.substr(sa[i]) << endl;
+      cout << i << ": " << s.substr(sa[i]) << '\n';
     }
   }
 };
