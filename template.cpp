606d90011d175b4f499044cd08997a6a16658e4b
Suu0313 <shuya_0313@docomo.ne.jp>
2021-06-12 15:49:10 +0900

    add set op

--- template.cpp
+++ template.cpp
@@ -63,20 +63,36 @@ struct Set : set<T> {
     set_union((*this).begin(), (*this).end(), s.begin(), s.end(), inserter(res, res.end()));
     return res;
   }
+  Set &operator|=(const Set &s){
+    for(auto&&e : s) this->insert(e);
+    return (*this);
+  }
   Set operator&(const Set &s) const {
     Set res;
     set_intersection((*this).begin(), (*this).end(), s.begin(), s.end(), inserter(res, res.end()));
     return res;
   }
+  Set operator&=(const Set &s){ return (*this) = (*this) & s; }
   Set operator^(const Set &s) const {
     Set res;
     set_symmetric_difference((*this).begin(), (*this).end(), s.begin(), s.end(), inserter(res, res.end()));
     return res;
   }
+  Set &operator^=(const Set &s){
+    for(auto&&e : s){
+      if(this->exist(e)) this->erase(e);
+      else this->insert(e);
+    }
+    return (*this);
+  }
   Set operator-(const Set &s) const {
     Set res;
     set_difference((*this).begin(), (*this).end(), s.begin(), s.end(), inserter(res, res.end()));
     return res;
+  }
+  Set &operator-=(const Set &s){
+    for(auto&&e : s) this->erase(e);
+    return (*this);
   }
   const T &operator[](int k) const {
     if(k >= 0) return *std::next(this->begin(), k);
