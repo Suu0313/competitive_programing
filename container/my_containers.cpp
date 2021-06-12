5e0de9f7c611c865efe97f4c8a259a5e36415f75
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-06-12 15:49:10 +0900

    add set op

--- container/my_containers.cpp
+++ container/my_containers.cpp
@@ -41,26 +41,40 @@ struct Set : set<T> {
     set_union((*this).begin(), (*this).end(), s.begin(), s.end(), inserter(res, res.end()));
     return res;
   }
+  Set &operator|=(const Set &s){
+    for(auto&&e : s) this->insert(e);
+    return (*this);
+  }
   Set operator&(const Set &s) const {
     Set res;
     set_intersection((*this).begin(), (*this).end(), s.begin(), s.end(), inserter(res, res.end()));
     return res;
   }
+  Set operator&=(const Set &s){ return (*this) = (*this) & s; }
   Set operator^(const Set &s) const {
     Set res;
     set_symmetric_difference((*this).begin(), (*this).end(), s.begin(), s.end(), inserter(res, res.end()));
     return res;
+  }
+  Set &operator^=(const Set &s){
+    for(auto&&e : s){
+      if(this->exist(e)) this->erase(e);
+      else this->insert(e);
+    }
+    return (*this);
   }
   Set operator-(const Set &s) const {
     Set res;
     set_difference((*this).begin(), (*this).end(), s.begin(), s.end(), inserter(res, res.end()));
     return res;
   }
-  const T operator[](int k) const {
+  Set &operator-=(const Set &s){
+    for(auto&&e : s) this->erase(e);
+    return (*this);
+  }
+  const T &operator[](int k) const {
     if(k >= 0) return *std::next(this->begin(), k);
     else return *std::prev(this->end(), -k);
   }
-  bool exist(const T &x) const {
-    return (*this).find(x) != (*this).end();
-  }
+  bool exist(const T &x) const { return (*this).find(x) != (*this).end(); }
 };
