9d00d411ed74fa63f349493b54342ee466974c13
Suu0313 <shuya_0313@docomo.ne.jp>
2021-06-16 17:48:37 +0900

    update roughly

--- math/Fraction.cpp
+++ math/Fraction.cpp
@@ -1,103 +1,90 @@
 struct Fraction{
-  LL a,b;
-  Fraction normalize(){
-    if(b < 0){ a = -a; b = -b;
-    }
-    LL g = gcd(a, b);
+  long long a,b;
+  constexpr Fraction normalize(){
+    if(b < 0){ a = -a; b = -b; }
+    long long g = gcd(a, b);
     if(g == 0){
       a = 0; b = 1;
     }else{
       a /= g; b /= g;
     }
-    return *this;
+    return (*this);
   }
-  Fraction(LL a=0, LL b=1): a(a),b(b){
-    normalize();
-  }
+  constexpr Fraction(long long a=0, long long b=1): a(a), b(b) { normalize(); }
   Fraction(string S){
     S += ".0";
     string T;
     vector<string> st;
     stringstream ss{S};
-    while(getline(ss,T,'.')) st.PB(T);
-    LL A = stoll(st.at(0));
-    LL B = stoll(st.at(1));
-    int C = SZ(st.at(1));
-    LL D = 1;
-    REP(i,C) D *= 10;
-    A = A*D+B;
-    a = A, b = D;
+    while(getline(ss,T,'.')) st.emplace_back(T);
+    long long A = stoll(st[0]), B = stoll(st[1]), D = 1;
+    for(size_t i = 0; i < st[1].size(); i++) D *= 10;
+    A = A*D+B; a = A; b = D;
     normalize();
-    if(S.at(0) == '-' && a>0) a = -a;
+    if(S[0] == '-' && a>0) a = -a;
   }
-  double QtoR() const {
-    return (double)(a) / b;
-  }
-  LL QtoZ() const {
-    return a/b;
-  }
+  constexpr double QtoR() const { return (double)(a) / b; }
+  constexpr long long QtoZ() const { return a / b; }
 
-  Fraction &operator+=(const Fraction &p){
-    LL l = lcm(b, p.b);
+  constexpr Fraction &operator+=(const Fraction &p){
+    long long l = lcm(b, p.b);
     a = a*(l/b) + p.a*(l/p.b);
     b = l;
     normalize();
     return *this;
   }
-  Fraction &operator-=(const Fraction &p){
-    LL l = lcm(b, p.b);
+  constexpr Fraction &operator-=(const Fraction &p){
+    long long l = lcm(b, p.b);
     a = a*(l/b) - p.a*(l/p.b);
     b = l;
     normalize();
     return *this;
   }
-  Fraction &operator*=(const Fraction &p){
+  constexpr Fraction &operator*=(const Fraction &p){
     Fraction x(a, p.b), y(p.a, b);
     a = x.a * y.a;
     b = x.b * y.b;
     normalize();
     return *this;
   }
-  Fraction &operator/=(const Fraction &p){
-    *this *= p.inverse();
-    return *this;
+  constexpr Fraction &operator/=(const Fraction &p){
+    return (*this) *= p.inverse();
   }
-  Fraction operator-() const {
+  constexpr Fraction operator-() const {
     return Fraction(-a, b);
   }
-  Fraction &operator=(LL a){
-    *this = Fraction(a, 1);
-    return *this;
+  constexpr Fraction &operator=(long long a){
+    return (*this) = Fraction(a, 1);
   }
-  Fraction operator+(const Fraction &p) const {
+  constexpr Fraction operator+(const Fraction &p) const {
     return Fraction(*this) += p;
   }
-  Fraction operator-(const Fraction &p) const {
+  constexpr Fraction operator-(const Fraction &p) const {
     return Fraction(*this) -= p;
   }
-  Fraction operator*(const Fraction &p) const {
+  constexpr Fraction operator*(const Fraction &p) const {
     return Fraction(*this) *= p;
   }
-  Fraction operator/(const Fraction &p) const {
+  constexpr Fraction operator/(const Fraction &p) const {
     return Fraction(*this) /= p;
   }
-  bool operator==(const Fraction &p) const {
+  constexpr bool operator==(const Fraction &p) const {
     return (a==p.a)&&(b==p.b);
   }
-  bool operator!=(const Fraction &p) const {
+  constexpr bool operator!=(const Fraction &p) const {
     return (a!=p.a)||(b!=p.b);
   }
 
-  Fraction inverse() const {
+  constexpr Fraction inverse() const {
     return Fraction(b, a);
   }
-  Fraction abs(const Fraction &p){
+  constexpr Fraction abs(const Fraction &p){
     Fraction res = p;
     if(res.a < 0) res.a = -res.a;
     return res;
   }
 
-  bool operator<(const Fraction &p) const {
+  constexpr bool operator<(const Fraction &p) const {
     return a * p.b < p.a * b;
   }
 
