011cd968d5423eeca0f2e197f24f75c424a4504a
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-06-22 10:17:13 +0900

    uodate ijk

--- graph/Dijkstra.cpp
+++ graph/Dijkstra.cpp
@@ -10,8 +10,27 @@ vector<T> Dijkstra(const Graph<T> &g, int s){
     seen[v] = true;
     for(auto&&e : g[v]){
       if(dist[e.to]==-1 || dist[e.to]>d+e.cost){
-        dist[e.to] = d + e.cost;
-        pq.emplace(dist[e.to], e.to);
+        pq.emplace(dist[e.to] = d + e.cost, e.to);
+      }
+    }
+  }
+  return dist;
+}
+
+template<typename T, typename T2, typename F>
+vector<T> Dijkstra(const Graph<T2> &g, int s, const F &f){
+  priority_queue<pair<T, int>,vector<pair<T, int>>, greater<>> pq;
+  vector<T> dist(g.size(), -1);
+  vector<bool> seen(g.size(), false);
+  pq.emplace(0, s); dist[s] = 0;
+  while(!pq.empty()){
+    auto[d,v] = pq.top(); pq.pop();
+    if(seen[v]) continue;
+    seen[v] = true;
+    for(auto&&e : g[v]){
+      auto nd = f(d, e);
+      if(dist[e.to]==-1 || dist[e.to]>nd){
+        pq.emplace(dist[e.to] = nd, e.to);
       }
     }
   }
