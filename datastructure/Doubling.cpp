ed56307288b778f215674c1c8a316320b9d58332
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-08-11 23:26:37 +0900

    Update Doubling.cpp

--- datastructure/Doubling.cpp
+++ datastructure/Doubling.cpp
@@ -5,9 +5,10 @@ struct Doubling{
   vector<vector<int>> npos;
   vector<vector<T>> val;
   const OP op;
+  bool is_built;
 
   Doubling(size_t n, const OP &op)
-    : npos(bwidth, vector<int>(n, -1)), val(bwidth, vector<T>(n)), op(op) {}
+    : npos(bwidth, vector<int>(n, -1)), val(bwidth, vector<T>(n)), op(op), is_built(false) {}
 
   void set_next(size_t i, int p){ npos[0][i] = p; }
   void set_value(size_t i, const T &x){ val[0][i] = x; }
@@ -15,6 +16,7 @@ struct Doubling{
   void set_value(const vector<T> &v){ val[0] = v; }
 
   void build(){
+    is_built = true;
     size_t n = npos[0].size();
     for(size_t d = 0; d+1 < bwidth; d++){
       for(size_t i = 0; i < n; i++){
@@ -25,7 +27,8 @@ struct Doubling{
     }
   }
 
-  pair<int, T> query(int s, uint64_t k, const T &id = T()) const {
+  pair<int, T> query(int s, uint64_t k, const T &id = T()) {
+    if(!is_built) build();
     T res = id;
     for(int d = bwidth-1; d >= 0; d--){
       if((k>>d) & 1){
