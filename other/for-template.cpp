c4e678ee495da0f21a96bdd1b4b249d6779854a0
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-08-12 22:57:12 +0900

    add patition ( to n subsequence)

--- other/for-template.cpp
+++ other/for-template.cpp
@@ -87,3 +87,25 @@ struct product{
   itr begin(){ return {0, vector<int>(n), this}; }
   monostate end() { return {}; }
 };
+
+struct patition{
+  struct itr{
+    uint64_t s;
+    int n;
+    bool operator!=([[maybe_unused]] monostate e){ return s != 1ull<<n; }
+    void operator++(){ ++s; }
+    vector<vector<int>> operator*(){
+      vector<vector<int>> res(1, vector<int>(1, 0));
+      for(int i = 0; i < n; ++i){
+        if(s>>i & 1) res.emplace_back(1, i+1);
+        else res.back().emplace_back(i+1);
+      }
+      return res;
+    }
+  };
+  int n;
+  patition(int n): n(n) {}
+
+  itr begin(){ return {0, n-1}; }
+  monostate end() { return {}; }
+};
