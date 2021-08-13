78edd092b038cea5bf545d361a4ffb9b9ff75791
Suu0313 <shuya_0313@docomo.ne.jp>
2021-08-13 11:35:26 +0900

    Update CumulativeSum.cpp

--- datastructure/CumulativeSum.cpp
+++ datastructure/CumulativeSum.cpp
@@ -1,12 +1,12 @@
 template<typename T>
 struct CumulativeSum{
   int n, m;
+  bool is_built;
   vector<vector<T>> sum;
-  CumulativeSum(int n, int m) : n(n), m(m) {
-    sum.assign(n+1, vector<T>(m+1, 0));
-  }
-  CumulativeSum(const vector<vector<T>> &v, bool isbuilded = false){
-    if(!isbuilded){
+  CumulativeSum(int n, int m) : n(n), m(m), is_built(false), sum(n+1, vector<T>(m+1)) {}
+  CumulativeSum(const vector<vector<T>> &v, bool isbuilt = false){
+    is_built = isbuilt;
+    if(!isbuilt){
       n = v.size(); m = v.at(0).size();
       sum.assign(n+1, vector<T>(m+1, 0));
       for(int i=0; i<n; i++){
@@ -25,6 +25,7 @@ struct CumulativeSum{
   const vector<T> &at(int k) const { return sum.at(k); }
 
   void build(){
+    is_built = true;
     for(int i=0; i<n; i++){
       for(int j=0; j<m; j++){
         sum.at(i+1).at(j+1) += sum.at(i+1).at(j);
@@ -37,6 +38,7 @@ struct CumulativeSum{
     }
   }
   T query(int x1, int x2, int y1, int y2){
+    if(!is_built) build();
     return sum.at(x2).at(y2)-sum.at(x1).at(y2)-sum.at(x2).at(y1)+sum.at(x1).at(y1);
   }
 };
