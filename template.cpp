20e6f2c01fd34afa683a2285da7e43a3a6ceeaa5
Suu0313 <shuya_0313@docomo.ne.jp>
2021-09-14 01:05:09 +0900

    update

--- template.cpp
+++ template.cpp
@@ -156,36 +156,6 @@ template<class T1, class T2> pair<T1,T2> &operator--(pair<T1, T2> &p){ --p.first
 template<typename T> vector<T> make_v(size_t a,T b){ return vector<T>(a, b); }
 template<typename... Ts> auto make_v(size_t a,Ts... ts){ return vector(a,make_v(ts...)); }
 
-template<typename T> valarray<T> make_va(size_t a,T b){ return valarray<T>(b, a); }
-template<typename... Ts> auto make_va(size_t a,Ts... ts){ return valarray(make_va(ts...), a); }
-
-template<typename T>
-vector<tuple<T>> Zip(vector<T> &v){
-  vector<tuple<T>> vt(v.size());
-  for(size_t i = 0; i < v.size(); i++) vt[i] = make_tuple(v[i]);
-  return vt;
-}
-template<typename T, typename... Ts>
-auto Zip(vector<T> &v, Ts&&... vs){
-  auto vt = Zip(v); auto vts = Zip(vs...);
-  size_t m = min(vt.size(), vts.size());
-  auto te = decltype(vt)(1)[0]; auto tse = decltype(vts)(1)[0];
-  vector res(m, tuple_cat(te, tse));
-  for(size_t i = 0; i < m; i++) res[i] = tuple_cat(vt[i], vts[i]);
-  return res;
-}
-
-template<typename T>
-vector<tuple<int, T>> Enumerate(vector<T> &v){
-  vector<int> idx(v.size()); iota(idx.begin(), idx.end(), 0);
-  return Zip(idx, v);
-}
-template<typename T, typename... Ts>
-auto Enumerate(vector<T> &v, Ts&&... vs){
-  vector<int> idx(v.size()); iota(idx.begin(), idx.end(), 0);
-  return Zip(idx, v, vs...);
-}
-
 template<typename Container>
 constexpr int SZ(const Container &c){ return size(c); }
 template<typename Container>
