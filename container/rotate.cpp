46ee6e2eee4b62b37be7c7e0f6b7578e583389db
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-09-28 11:34:46 +0900

    Create rotate.cpp

---
+++ container/rotate.cpp
@@ -0,0 +1,11 @@
+template<typename T>
+vector<vector<T>> rotate(const vector<vector<T>> &a){
+  int h = int(a.size()), w = int(a[0].size());
+  vector<vector<T>> b(w, vector<T>(h));
+  for(int i = 0; i < h; ++i){
+    for(int j = 0; j < w; ++j){
+      b[j][h-i-1] = a[i][j];
+    }
+  }
+  return b;
+}
