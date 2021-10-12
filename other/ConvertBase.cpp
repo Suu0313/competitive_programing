3944e792ac9e50a4bfe5341cab797b68a7c88fe3
Suu0313 <shuya_0313@docomo.ne.jp>
2021-10-12 09:42:48 +0900

    Create ConvertBase.cpp

---
+++ other/ConvertBase.cpp
@@ -0,0 +1,21 @@
+template<typename T, typename T2>
+vector<T> convert_base_10tob(T x, T2 b) {
+  vector<T> ret;
+  T t = 1, k = abs(b);
+  while(x) {
+    ret.emplace_back((x * t) % k);
+    if(ret.back() < 0) ret.back() += k;
+    x -= ret.back() * t;
+    x /= k; t *= b / k;
+  }
+  if(ret.empty()) ret.emplace_back(0);
+  reverse(begin(ret), end(ret));
+  return ret;
+}
+
+template<typename T, typename T2>
+T convert_base_bto10(const vector<T> &a, T2 b) {
+  T ret = 0;
+  for(auto&e : a) (ret *= b) += e;
+  return ret;
+}
