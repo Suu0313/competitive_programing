e0c3b8b8cb06c173196656713ea12162c0511191
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-11-18 20:45:20 +0900

    update

---
+++ dp/largest_rectangle.cpp
@@ -0,0 +1,31 @@
+template<typename T>
+T largest_rectangle(vector<T> height){
+  stack<int> st;
+  height.push_back(0);
+  vector<int> left(height.size());
+  T res = 0;
+  for(int i = 0; i < int(height.size()); i++) {
+    while(!st.empty() && height[st.top()] >= height[i]) {
+      res = max(res, (i - left[st.top()] - 1) * height[st.top()]);
+      st.pop();
+    }
+    left[i] = st.empty() ? -1 : st.top();
+    st.emplace(i);
+  }
+  return res;
+}
+
+template<typename T>
+int largest_rectangle(const vector<vector<T>> &tl){
+  int w = int(tl[0].size());
+  vector<int> dp(w);
+  int ans = 0;
+  for(auto&&v : tl){
+    for(int i = 0; i < w; ++i){
+      if(v[i]) dp[i] = 0;
+      else ++dp[i];
+    }
+    ans = max(ans, largest_rectangle(dp));
+  }
+  return ans;
+}
---
+++ dp/largest_square.cpp
@@ -0,0 +1,18 @@
+template<typename T>
+int largest_square(const vector<vector<T>> &tl){
+  int h = int(tl.size()), w = int(tl[0].size());
+  vector<int> dp(w);
+  for(int i = 0; i < w; ++i) dp[i] = !tl[0][i];
+  int ans = *max_element(begin(dp), end(dp));
+
+  for(int i = 1; i < h; ++i){
+    vector<int> ndp(w); swap(dp, ndp);
+    dp[0] = !tl[i][0];
+    for(int j = 1; j < w; ++j){
+      if(tl[i][j]) continue;
+      dp[j] = min({dp[j-1], ndp[j], ndp[j-1]}) + 1;
+    }
+    ans = max(ans, *max_element(begin(dp), end(dp)));
+  }
+  return ans;
+}
