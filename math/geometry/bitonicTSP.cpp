e1fef936c9021e105d49e1d356b06e557a77292f
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2021-11-19 09:00:30 +0900

    update

---
+++ math/geometry/bitonicTSP.cpp
@@ -0,0 +1,18 @@
+template<typename T>
+double bitonicTSP(const Polygon<T> &ps){
+  int n = int(ps.size());
+  vector dp(n, vector(n, 0.0));
+  for(int j = 1; j < n; ++j){
+    for(int i = 0; i < j; ++i){
+      if(i == 0 && j == 1) dp[i][j] = ps[i].dist(ps[j]);
+      else if(i < j-1) dp[i][j] = dp[i][j-1] + ps[j-1].dist(ps[j]);
+      else{
+        dp[i][j] = numeric_limits<double>::infinity();
+        for(int k = 0; k < i; ++k) dp[i][j] = min(dp[i][j], dp[k][i] + ps[j].dist(ps[k]));
+      }
+    }
+  }
+  double ans = numeric_limits<double>::infinity();
+  for(int k = 0; k < n-1; ++k) ans = min(ans, dp[k][n-1] + ps[k].dist(ps[n-1]));
+  return ans;
+}
