c07383a8a761d8515ec2d5c75f32cd6b7f7fffef
Suu0313 <shuya_0313@docomo.ne.jp>
2022-02-07 00:11:20 +0900

    fix to bellows

--- other/MoSort.cpp
+++ other/MoSort.cpp
@@ -1,9 +1,9 @@
 vector<int> MoSort(int n, int q, const vector<int> &l, const vector<int> &r){
   vector<int> idx(q); iota(begin(idx), end(idx), 0);
   int d = max(1.0, n/sqrt(q));
-  sort(begin(idx), end(idx), [&](int i, int j){
+  sort(begin(idx), end(idx), [&](int i, int j)->bool{
     if(l[i]/d != l[j]/d) return l[i] < l[j];
-    return r[i] < r[j];
+    return (r[i] < r[j])^(l[i]/d&1);
   });
   return idx;
 }
