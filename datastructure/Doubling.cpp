94d6681be2652565ddb4e13a7a3eb61c1f69c033
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-02-28 18:03:16 +0900

    add only pos ver

--- datastructure/Doubling.cpp
+++ datastructure/Doubling.cpp
@@ -39,3 +39,35 @@ struct Doubling{
     return {s, res};
   }
 };
+
+
+template<size_t bwidth = 60>
+struct Doubling{
+
+  vector<vector<int>> npos;
+  bool is_built;
+
+  Doubling(size_t n): npos(bwidth, vector<int>(n, -1)), is_built(false) {}
+
+  void set_next(size_t i, int p){ npos[0][i] = p; }
+  void set_next(const vector<int> &p){ npos[0] = p; }
+
+  void build(){
+    is_built = true;
+    size_t n = npos[0].size();
+    for(size_t d = 0; d+1 < bwidth; d++){
+      for(size_t i = 0; i < n; i++){
+        int p = npos[d][i]; if(p == -1) continue;
+        npos[d+1][i] = npos[d][p];
+      }
+    }
+  }
+
+  int query(int s, uint64_t k) {
+    if(!is_built) build();
+    for(int d = bwidth-1; d >= 0; d--){
+      if((k>>d) & 1) s = npos[d][s];
+    }
+    return s;
+  }
+};
