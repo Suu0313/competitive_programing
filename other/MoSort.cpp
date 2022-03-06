c9f2c38f57a94d9159aa0ad7970a234a542af8d7
Suu0313 <shuya_0313@docomo.ne.jp>
2022-03-06 17:57:53 +0900

    bug fixed !!

--- other/MoSort.cpp
+++ other/MoSort.cpp
@@ -3,7 +3,7 @@ vector<int> MoSort(int n, int q, const vector<int> &l, const vector<int> &r){
   int d = max<int>(1, n/max<double>(1.0, sqrt(q*2.0/3.0)));
   sort(begin(idx), end(idx), [&](int i, int j){
     if(l[i]/d != l[j]/d) return l[i] < l[j];
-    return r[i] < r[j];
+    else return ((l[i]/d)%2 ? (r[i] < r[j]) : (r[i] > r[j]));
   });
   return idx;
 }
