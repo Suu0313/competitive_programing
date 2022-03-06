f3f3f29ddb1defb856814fc71e4e55d216dfccf8
Suu0313 <shuya_0313@docomo.ne.jp>
2022-03-06 17:38:09 +0900

    bug fix?

--- other/MoSort.cpp
+++ other/MoSort.cpp
@@ -1,9 +1,9 @@
 vector<int> MoSort(int n, int q, const vector<int> &l, const vector<int> &r){
   vector<int> idx(q); iota(begin(idx), end(idx), 0);
   int d = max<int>(1, n/max<double>(1.0, sqrt(q*2.0/3.0)));
-  sort(begin(idx), end(idx), [&](int i, int j)->bool{
+  sort(begin(idx), end(idx), [&](int i, int j){
     if(l[i]/d != l[j]/d) return l[i] < l[j];
-    return (r[i] < r[j])^(l[i]/d&1);
+    return r[i] < r[j];
   });
   return idx;
 }
