4be944b9fcc9a3176fd739069267db137a74b6c0
Suu0313 <shuya_0313@docomo.ne.jp>
2022-03-08 16:12:12 +0900

    create

---
+++ math/quotient_range.cpp
@@ -0,0 +1,11 @@
+template<typename T>
+vector<tuple<T, T, T>> quotient_range(T n){
+  vector<tuple<T, T, T>> res;
+  T l = 1;
+  while(l <= n){
+    T d = n/l, r = n/d + 1;
+    res.emplace_back(l, r, d);
+    l = r;
+  }
+  return res;
+}
