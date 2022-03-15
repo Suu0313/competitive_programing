153093df3c5228fb115b1725e8f08dfaca076c8a
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-03-15 17:40:44 +0900

    update

--- string/KnuthMorrisPrattAlgorithm.cpp
+++ string/KnuthMorrisPrattAlgorithm.cpp
@@ -1,26 +1,56 @@
-vector<int> KnuthMorrisPrattAlgorithm(const string &s){
-  int n = int(s.size());
-  vector<int> a(n+1, -1);
-  for(int i = 0; i < n; ){
-    int j = a[i];
-    while(j >= 0 && (s[i] != s[j])) j = a[j];
-    ++i; ++j;
-    if(s[i] == s[j]) a[i] = a[j];
-    else a[i] = j;
-  }
-  return a;
-}
+template<typename Container>
+struct KnuthMorrisPrattAlgorithm{
+  Container s;
+  vector<int> mp, kmp;
+  KnuthMorrisPrattAlgorithm() = default;
 
-template<typename T>
-vector<int> KnuthMorrisPrattAlgorithm(const vector<T> &s){
-  int n = int(s.size());
-  vector<int> a(n+1, -1);
-  for(int i = 0; i < n; ){
-    int j = a[i];
-    while(j >= 0 && (s[i] != s[j])) j = a[j];
-    ++i; ++j;
-    if(s[i] == s[j]) a[i] = a[j];
-    else a[i] = j;
+  KnuthMorrisPrattAlgorithm(const Container &s): s(s), mp(s.size()+1, -1), kmp(s.size()+1, -1){
+    int n = int(s.size());
+    for(int i = 0, j = -1; i < n; ){
+      while(j >= 0 && (s[i] != s[j])) j = kmp[j];
+      ++i; ++j;
+      mp[i] = j;
+      if(s[i] == s[j]) kmp[i] = kmp[j];
+      else kmp[i] = j;
+    }
   }
-  return a;
-}
+
+  template<typename T>
+  void push_back(T c){
+    int i = int(s.size()), j = mp[i];
+    s.push_back(c);
+    kmp.pop_back();
+    kmp.push_back(((j >= 0) && (s[i] == s[j])) ? kmp[j] : j);
+
+
+    while(j >= 0 && (s[i] != s[j])) j = kmp[j];
+    ++i; ++j;
+    mp.push_back(j);
+    kmp.push_back(j);
+  }
+
+  void pop_back(){
+    s.pop_back();
+    mp.pop_back();
+    kmp.pop_back();
+    kmp.back() = mp.back();
+  }
+
+  // period of [0, i)
+  int period(int i) const { return i - mp[i]; }
+
+  // t[i:i+n] == s
+  vector<int> match(const Container &t) const {
+    int n = int(s.size()), m = int(t.size());
+    vector<int> res;
+
+    for(int i = 0, j = 0; j < m; ){
+      while(i >= 0 && (s[i] != t[j])) i = kmp[i];
+      ++i; ++j;
+      if(i >= n){
+        res.push_back(j-i); i = kmp[i];
+      }
+    }
+    return res;
+  }
+};
