c024bf7b89d276cc1b2f3fd8d020726466cc20cd
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-03-16 00:22:47 +0900

    create

---
+++ math/convolution/superset_fast_moebius_transform.cpp
@@ -0,0 +1,10 @@
+template<typename T, class Minus = minus<T>>
+vector<T> superset_fast_moebius_transform(vector<T> f, const Minus &op = Minus{}){
+  int n = int(f.size());
+  for(int i = 1; i < n; i <<= 1){
+    for(int j = 0; j < n; ++j){
+      if((i&j) == 0) f[j] = op(f[j], f[j | i]);
+    }
+  }
+  return f;
+}
---
+++ math/convolution/superset_fast_zeta_transform.cpp
@@ -0,0 +1,10 @@
+template<typename T, class Plus = plus<T>>
+vector<T> superset_fast_zeta_transform(vector<T> f, const Plus &op = Plus{}){
+  int n = int(f.size());
+  for(int i = 1; i < n; i <<= 1){
+    for(int j = 0; j < n; ++j){
+      if((i&j) == 0) f[j] = op(f[j], f[j | i]);
+    }
+  }
+  return f;
+}
