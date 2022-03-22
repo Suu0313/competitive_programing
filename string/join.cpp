47fb61571fbaebbefd1f156593d3bf5298d93532
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-03-22 22:48:13 +0900

    create

---
+++ string/join.cpp
@@ -0,0 +1,6 @@
+template<typename Container>
+string join(const Container&v, const string &sep = "", const string &en = ""){
+  stringstream s;
+  for(size_t i = 0; i < size(v); i++) s << v[i] << (i+1 != size(v) ? sep : en);
+  return s.str();
+}
