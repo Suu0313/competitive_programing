22178ff9c0a8758a5b706b7eba3eea828101bf4b
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-03-29 07:00:08 +0900

    create

---
+++ math/convolution/multiple_fast_moebius_transform.cpp
@@ -0,0 +1,13 @@
+template<typename T, class Minus = minus<T>>
+vector<T> multiple_fast_moebius_transform(vector<T> f, const Minus &op = Minus{}){
+  int n = int(f.size());
+  vector<bool> sieve(n, true);
+  for(int p = 2; p < n; ++p){
+    if(!sieve[p]) continue;
+    for(int k = 1; p * k < n; ++k){
+      sieve[p * k] = false;
+      f[k] = op(f[k], f[p * k]);
+    }
+  }
+  return f;
+}
