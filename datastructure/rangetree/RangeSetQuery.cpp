e7325f1b9817e46faa3dec97b4091552ed920b30
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-04-13 19:55:16 +0900

    todo

--- datastructure/rangetree/RangeSetQuery.cpp
+++ datastructure/rangetree/RangeSetQuery.cpp
@@ -17,3 +17,23 @@ struct RangeSetQuery{
     return r - l - seg.query(l, r, l, r);
   }
 };
+
+// ToDo: WM で書き直す
+
+int main(){
+  cin.tie(0); ios::sync_with_stdio(false);
+  int n, q; cin >> n >> q;
+  vector<int> c(n); for(auto&&e : c) cin >> e, --e;
+  vector<int> a(n), pre(n, n);
+  for(int i = 0; i < n; i++){
+    a[i] = pre[c[i]];
+    pre[c[i]] = i;
+  }
+
+  WaveletMatrix<20, int> wm(a);
+
+  while(q--){
+    int l, r; cin >> l >> r; --l;
+    cout << r - l - wm.rank(l, r, l, r) << "\n";
+  }
+}
