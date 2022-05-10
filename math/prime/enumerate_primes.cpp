1682ed0800f3bec3a57f4316b430d6e90fbc06fa
Suu0313 <shuya_0313@docomo.ne.jp>
2022-05-10 22:27:11 +0900

    update

--- math/prime/enumerate_primes.cpp
+++ math/prime/enumerate_primes.cpp
@@ -1,14 +1,14 @@
 vector<int> enumerate_primes(int n){
   vector<bool> table(n + 1, 1);
   vector<int> ps;
-
-  for(int i = 2; i <= n; ++i){
-    if(table[i]) ps.push_back(i);
-    for(int p : ps){
-      if(p * int64_t(i) > n) break;
-      table[p * i] = 0;
-    }
+  int i = 2;
+  for(; i * i <= n; ++i){
+    if(!table[i]) continue;
+    ps.push_back(i);
+    for(int j = i * i; j <= n; j += i) table[j] = 0;
   }
+
+  for(; i <= n; ++i) if(table[i]) ps.push_back(i);
 
   return ps;
 }
