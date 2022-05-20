2f98c84415d72a0bad30a83a498338c156f7cf31
Suu0313 <shuya_0313@docomo.ne.jp>
2022-05-21 03:28:53 +0900

    update

--- math/binomial_table.cpp
+++ math/binomial_table.cpp
@@ -1,9 +1,11 @@
 template<typename T>
-vector<vector<T>> binomial_table(int N){
-  vector<vector<T>> mat(N+1, vector<T>(N+1));
-  CREP(i,N)CREP(j,i){
-    if(j==0 || j==i) mat.at(i).at(j) = 1;
-    else mat.at(i).at(j) = mat.at(i-1).at(j-1) + mat.at(i-1).at(j);
+vector<vector<T>> binomial_table(int n){
+  vector<vector<T>> mat(n+1, vector<T>(n+1));
+  for(int i = 0; i <= n; ++i){
+    for(int j = 0; j <= i; ++j){
+      if(j == 0 || j == i) mat[i][j] = 1;
+      else mat[i][j] = mat[i-1][j-1] + mat[i-1][j];
+    }
   }
   return mat;
 }
