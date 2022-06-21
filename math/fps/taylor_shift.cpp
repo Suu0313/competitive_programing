467757ab878e8d9a28cc8c5ff9dec4b7b523e785
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-06-21 12:39:46 +0900

    create

---
+++ math/fps/taylor_shift.cpp
@@ -0,0 +1,20 @@
+template< typename T >
+Formalpowerseries<T> taylor_shift(const Formalpowerseries<T> &f, const T &c){
+  int n = int(f.size()), mod = T::get_mod();
+  vector<T> finv(n, 1), fact(n, 1);
+  for(int i = 2; i < n; ++i){
+    fact[i] = fact[i - 1] * i;
+    finv[i] = -finv[mod % i] * (mod / i);
+  }
+
+  for(int i = 3; i < n; ++i) finv[i] *= finv[i - 1];
+
+  Formalpowerseries<T> rf(n), exp_cx(n, 1);
+  for(int i = 0; i < n; ++i) rf[n - i - 1] = f[i] * fact[i];
+  for(int i = 1; i < n; ++i) exp_cx[i] = exp_cx[i - 1] * c;
+  for(int i = 2; i < n; ++i) exp_cx[i] *= finv[i];
+
+  Formalpowerseries<T> g = (rf * exp_cx).pre(n).rev();
+  for(int i = 2; i < n; ++i) g[i] *= finv[i];
+  return g;
+}
