4dd0b37655cd02b283637ec2b6e002e883f989bb
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-06-23 03:44:30 +0900

    create

---
+++ math/fps/polynomial_interpolation.cpp
@@ -0,0 +1,26 @@
+template<typename T>
+Formalpowerseries<T> polynomial_interpolation(const vector<T> &xs, const vector<T> &ys){
+  using fps = Formalpowerseries<T>;
+  int n = int(xs.size());
+  
+  queue<fps> fs;
+  for(auto&&x : xs) fs.push({-x, 1});
+  while(int(fs.size()) > 1){
+    fps f = fs.front(); fs.pop();
+    fs.push(f * fs.front()); fs.pop();
+  }
+  fps g = fs.front();
+  vector<T> dys = multipoint_evaluation(g.diff(), xs);
+  
+  queue<pair<fps, fps>> qu;
+  for(int i = 0; i < n; ++i){
+    qu.emplace(fps{ys[i] / dys[i]}, fps{-xs[i], 1});
+  }
+  while(int(qu.size()) > 1){
+    auto[f1, g1] = qu.front(); qu.pop();
+    auto[f2, g2] = qu.front(); qu.pop();
+    qu.emplace(f1 * g2 + f2 * g1, g1 * g2);
+  }
+  auto[f1, g1] = qu.front();
+  return (g * f1 * g1.inv()).pre(n);
+}
---
+++ math/fps/stirling_number_of_the_second_kind.cpp
@@ -0,0 +1,11 @@
+template<typename T>
+Formalpowerseries<T> stirling_number_of_the_second_kind(int n){
+  int mod = T::get_mod();
+  Formalpowerseries<T> f(n + 1, 1);
+  for(int i = 2; i <= n; ++i) f[i] = -f[mod % i] * (mod / i);
+  for(int i = 3; i <= n; ++i) f[i] *= f[i - 1];
+  Formalpowerseries<T> g = f;
+  for(int i = 0; i <= n; ++i) f[i] *= T(i).pow(n);
+  for(int i = 1; i <= n; i += 2) g[i] = -g[i];
+  return (f * g).pre(n + 1);
+}
