a84d22a5b53b8ea4d63ef0b22f22bbbf5fef1731
Suu0313 <shuya_0313@docomo.ne.jp>
2022-06-30 03:26:21 +0900

    create

---
+++ other/make_ref_vector.cpp
@@ -0,0 +1,21 @@
+template<typename T>
+auto make_ref_vector(vector<T> &v, const vector<int> &idx){
+  vector<reference_wrapper<T>> res;
+  for(int i : idx) res.push_back(v[i]);
+  return res;
+}
+
+template<typename T>
+auto make_ref_vector(vector<T> &v, initializer_list<int> idx){
+  return make_ref_vector(v, idx);
+}
+
+template<typename T>
+auto make_ref_vector(vector<T> &v, const vector<bool> &use){
+  vector<reference_wrapper<T>> res;
+  int n = int(min(v.size(), use.size()));
+  for(int i = 0; i < n; ++i){
+    if(use[i]) res.push_back(v[i]);
+  }
+  return res;
+}
