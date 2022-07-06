71b4dc3c3ba38306896078f831f9296290070789
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-07-07 01:21:01 +0900

    create

---
+++ other/popcount.cpp
@@ -0,0 +1,7 @@
+constexpr int popcount(uint64_t x){
+  x -= ((x>>1)&0x5555555555555555ULL);
+  x = (x&0x3333333333333333ULL) + ((x&0xCCCCCCCCCCCCCCCCULL)>>2);
+  x = (x&0x0F0F0F0F0F0F0F0FULL) + ((x&0xF0F0F0F0F0F0F0F0ULL)>>4);  
+  x *= 0x0101010101010101ULL;
+  return x >> 56;
+}
