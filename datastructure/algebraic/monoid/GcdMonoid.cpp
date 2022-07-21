f0bc45fbadd8b94ed230d15283702ea27c25f3db
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-07-22 03:02:36 +0900

    create (no verify)

---
+++ datastructure/algebraic/monoid/GcdMonoid.cpp
@@ -0,0 +1,23 @@
+template<typename T>
+struct GcdMonoid{
+  T x = 0;
+  GcdMonoid() = default;
+  GcdMonoid(const GcdMonoid&) = default;
+  GcdMonoid(GcdMonoid&&) = default;
+
+  GcdMonoid(const T &a, const T &b): a(a), b(b) {}
+
+  GcdMonoid &operator=(const GcdMonoid&) = default;
+  GcdMonoid &operator=(GcdMonoid&&) = default;
+
+  GcdMonoid &operator+=(const GcdMonoid &m){
+    if(m.x != 0) x = gcd(x, m.x);
+    return (*this);
+  }
+  GcdMonoid operator+(const GcdMonoid &m) const {
+    return GcdMonoid(*this) += m;
+  }
+
+  bool operator==(const GcdMonoid &m) const { return x == m.x; }
+  bool operator!=(const GcdMonoid &m) const { return !((*this) == m); }
+};
