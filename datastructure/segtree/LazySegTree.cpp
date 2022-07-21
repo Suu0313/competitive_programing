292f77b575f82ffae9a6801206d22ddb3564a42e
Suu0313 <shuya_0313@docomo.ne.jp>
2022-07-22 00:06:01 +0900

    update

--- datastructure/segtree/LazySegTree.cpp
+++ datastructure/segtree/LazySegTree.cpp
@@ -1,181 +1,174 @@
-template<typename M, typename OM, typename F, typename G, typename H>
+template<class Operation>
 struct LazySegTree{
-  int n, defn, height;
-  vector<M> node;
-  vector<OM> lazy;
+  using Monoid = typename Operation::Monoid;
+  using OperatorMonoid = typename Operation::OperatorMonoid;
+
+  int n, height;
+  vector<Monoid> node;
+  vector<OperatorMonoid> lazy;
   vector<int> sz;
-  const F f;
-  const G g;
-  const H h;
-  const M m;
-  const OM om;
 
-  LazySegTree(int _n, const F &f, const G &g, const H &h, const M &m, const OM &om): f(f), g(g), h(h), m(m), om(om){
-    n = 1;
-    defn = _n;
-    height = 0;
-    while(n<defn) n<<=1, height++;
-    node.assign(2*n, m);
-    lazy.assign(n, om);
-    sz.assign(2*n, 1);
-    for(int k = n-1; k >= 0; k--) sz.at(k) = sz.at(2*k) + sz.at(2*k+1);
-  }
 
-  void set(int k, const M &x){ node.at(k + n) = x; }
-  
-  void build(){
-    for(int k = n-1; k >= 0; k--){ node.at(k) = f(node.at(2*k), node.at(2*k+1)); }
-  }
-
-  void build(const vector<M> &v){
-    assert((int)v.size() == defn);
-    for(int k = 0; k < defn; k++) node[k+n] = v[k];
+  LazySegTree() = default;
+  LazySegTree(int n): n(n), height(ilog2(n)), node(n * 2), lazy(n) {}
+  LazySegTree(int n, const Monoid &x): n(n), height(ilog2(n)), node(n * 2, x), lazy(n) { build(); }
+  template<typename Iiter>
+  LazySegTree(Iiter first, Iiter last): LazySegTree(int(distance(first, last))) {
+    for(int i = 0; i < n; ++i, ++first) node[i + n] = *first;
     build();
   }
+  template<class Container>
+  LazySegTree(const Container &c): LazySegTree(begin(c), end(c)) {}
 
-  void change(int k, const M &x){
-    k += n;
-    for(int i = height; i>=1; --i) push(k >> i);
-    node.at(k) = x;
-    for(int i=1; i <= height; i++) propagate(k >> i);
+
+  void build(){
+    for(int k = n-1; k >= 0; k--) propagate(k);
   }
 
-  void update(int k, const OM &x){
+  void set(int k, const Monoid &x){ node[k + n] = x; }
+
+  void change(int k, const Monoid &x){
     k += n;
-    for(int i = height; i>=1; --i) push(k >> i);
-    node.at(k) = g(node.at(k), x, sz.at(k));
-    for(int i=1; i <= height; i++) propagate(k >> i);
+    resolve(k);
+    node[k] = x;
+    build(k);
   }
 
-  void update(int l, int r, const OM &x){
+  void update(int k, const OperatorMonoid &x){
+    k += n;
+    resolve(k); apply(k, x); build(k);
+  }
+
+  void update(int l, int r, const OperatorMonoid &x){
     if(l == r) return;
     l += n; r += n;
+    int dl = l, dr = r;
+    resolve(dl); resolve(dr - 1);
 
-    for(int i = height; i>=1; --i){
-      if(((l>>i)<<i) != l) push(l >> i);
-      if(((r>>i)<<i) != r) push((r-1) >> i);
+    for(; l < r; l >>= 1, r >>= 1){
+      if(l & 1) apply(l++, x);
+      if(r & 1) apply(--r, x);
     }
 
-    {
-      int l2 = l, r2 = r;
-      while(l2 < r2){
-        if(l2&1) all_apply(l2++, x);
-        if(r2&1) all_apply(--r2, x);
-        l2 >>= 1;
-        r2 >>= 1;
-      }
-    }
-
-    for(int i = 1; i <= height; ++i){
-      if(((l>>i)<<i) != l) propagate(l >> i);
-      if(((r>>i)<<i) != r) propagate((r-1) >> i);
-    }
+    build(dl); build(dr - 1);
   }
 
-  M at(int k){
-    k += n;
-    for(int i = height; i>=1; --i) push(k >> i);
-    return node.at(k);
+  Monoid operator[](int k){
+    resolve[k += n];
+    return node[k];
   }
 
-  M operator[](int k){
-    k += n;
-    for(int i = height; i>=1; --i) push(k >> i);
-    return node.at(k);
-  }
-
-  M query(int l, int r){
-    if(l == r) return m;
+  Monoid query(int l, int r){
+    Monoid res_l{}, res_r{};
+    if(l == r) return res_l;
     l += n; r += n;
+    resolve(l); resolve(r - 1);
 
-    for(int i = height; i>=1; --i){
-      if(((l>>i)<<i) != l) push(l >> i);
-      if(((r>>i)<<i) != r) push(r >> i);
+    for(; l < r; l >>= 1, r >>= 1){
+      if(l & 1) res_l += node[l++];
+      if(r & 1) res_r = node[--r] + res_r;
     }
-
-    M sl = m, sr = m;
-    while(l < r){
-      if(l&1) sl = f(sl, node.at(l++));
-      if(r&1) sr = f(node.at(--r), sr);
-      l >>= 1;
-      r >>= 1;
-    }
-    return f(sl, sr);
+    return res_l += res_r;
   }
-
-  M all_query() const { return node.at(1); }
 
   template<typename C>
   int max_right(int l, C &check){
-    if(l == defn) return defn;
+    if(l == n) return n;
     l += n;
-    for(int i = height; i>=1; --i) push(l >> i);
-    M sm = m;
-    do{
-      while(l%2 == 0) l >>= 1;
-      if(!check(f(sm, node.at(l)))){
-        while(l < n){
-          push(l);
-          l *= 2;
-          if(check(f(sm, node.at(l)))){
-            sm = f(sm, node.at(l));
-            l++;
+    resolve(l);
+    Monoid x{};
+    int r = n * 2, h = 0;
+    for(; l < r; l >>= 1, r >>= 1, ++h){
+      if(l & 1){
+        if(!check(x + node[l])){
+          while(l < n){
+            l <<= 1;
+            if(check(x + node[l])) x += node[l++];
           }
+          return l - n;
         }
-        return l - n;
+        x += node[l++];
       }
-      sm = f(sm, node.at(l));
-      l++;
-    }while((l&-l) != l);
-    return defn;
+      if(r & 1) --r;
+    }
+
+    while(r <<= 1, h--){
+      if(((r + 1) << h) <= n * 2) {
+        if(!check(x + node[r])){
+          while(r < n){
+            r <<= 1;
+            if(check(x + node[r])) x += node[r++];
+          }
+          return r - n;
+        }
+        x += node[r++];
+      }
+    }
+
+    return n;
   }
 
   template<typename C>
   int max_left(int r, C &check){
     if(r == 0) return 0;
     r += n;
-    for(int i = height; i>=1; --i) push((r-1) >> i);
-    M sm = m;
-    do{
-      while(r>1 && (r%2)) r >>= 1;
-      if(!check(f(node.at(r), sm))){
-        while(r < n){
-          push(r);
-          r *= 2*r + 1;
-          if(check(f(node.at(r), sm))){
-            sm = f(node.at(r), sm);
-            r--;
+    resolve(r - 1);
+    Monoid x{};
+    int l = n, h = 0;
+    for(; l < r; l >>= 1, r >>= 1, ++h){
+      if(l & 1) ++l;
+      if(r & 1){
+        --r;
+        if(!check(node[r] + x)){
+          while(r < n){
+            r = 2*r + 1;
+            if(check(node[r] + x)) x = node[r--] + x;
           }
+          return r - n + 1;
         }
-        return r + 1 - n;
+        x = node[r] + x;
       }
-      sm = f(node.at(r), sm);
-    }while((r&-r) != r);
+    }
+
+    while(l <<= 1, h--){
+      if(((l - 1) << h) >= n){
+        --l;
+        if(!check(node[l] + x)){
+          while(l < n){
+            l = l * 2 + 1;
+            if(check(node[l] + x)) x = node[l--] + x;
+          }
+          return l - n + 1;
+        }
+        x = node[l] + x;
+      }
+    }
     return 0;
   }
 
 private:
-  void propagate(int k){ node.at(k) = f(node.at(2*k), node.at(2*k + 1));}
-  void all_apply(int k, OM x){
-    node.at(k) = g(node.at(k), x, sz.at(k));
-    if(k < n) lazy.at(k) = h(lazy.at(k), x);
+  int ilog2(int m){ return 31 - __builtin_clz(m); }
+  void propagate(int k){ node[k] = node[k<<1] + node[k<<1 | 1]; }
+  void build(int k){
+    while(k){
+      k >>= 1;
+      propagate(k);
+      Operation::act(node[k], lazy[k]);
+    }
   }
-  void push(int k){
-    if(lazy.at(k) == om) return;
-    all_apply(2*k, lazy.at(k));
-    all_apply(2*k + 1, lazy.at(k));
-    lazy.at(k) = om;
+  void apply(int k, const OperatorMonoid &x){
+    Operation::act(node[k], x);
+    if(k < n) lazy[k] += x;
+  }
+  void resolve(int k){
+    OperatorMonoid id{};
+    for(int h = height + 2; h > 0; --h){
+      int i = k >> h;
+      if(lazy[i] != id){
+        apply(i << 1, lazy[i]);
+        apply(i << 1 | 1, lazy[i]);
+        lazy[i] = id;
+      }
+    }
   }
 };
-
-template<typename M, typename OM, typename F, typename G, typename H>
-LazySegTree<M,OM,F,G,H> get_lazysegtree(int n, const F &f, const G &g, const H &h, const M &m, const OM &om){
-  return {n, f, g, h, m, om};
-}
-
-template<typename M, typename OM, typename F, typename G, typename H>
-LazySegTree<M,OM,F,G,H> get_lazysegtree(int n, const F &f, const G &g, const H &h, const M &m, const OM &om, const vector<M> &v){
-  LazySegTree<M,OM,F,G,H> seg(n, f, g, h, m, om);
-  seg.build(v);
-  return seg;
-}
