bac399e070d79f0a08b8169b9709a55d73e6bb89
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-07-22 21:37:10 +0900

    update

--- datastructure/sparsetable/DisjointSparseTable.cpp
+++ datastructure/sparsetable/DisjointSparseTable.cpp
@@ -1,38 +1,32 @@
-template<typename T>
+template<typename SemiGroup>
 struct DisjointSparseTable{
-  using F = function<T(T, T)>;
-  F f;
-  vector<vector<T>> table;
+  vector<vector<SemiGroup>> table;
   vector<int> lookup;
-  
-  DisjointSparseTable() {}
-  DisjointSparseTable(const vector<T> &v, const F &f): f(f){
-    const int n = v.size();
-    const int b = 32 - __builtin_clz(n);
-    table.assign(b, vector<T>(n));
-    for(int i = 0; i < n; i++) table.at(0).at(i) = v.at(i);
 
-    for(int i = 1; i < b; i++){
-      int shift = 1 << i;
-      for(int j = 0; j < n; j += shift << 1){
-        int t = min(j+shift, n);
-        table.at(i).at(t-1) = v.at(t-1);
-        for(int k = t-2; k >= j; k--) table.at(i).at(k) = f(v.at(k), table.at(i).at(k+1));
+  DisjointSparseTable() = default;
+  template<class Iiter>
+  DisjointSparseTable(Iiter first, Iiter last): table(1, vector<SemiGroup>(first, last)){
+    size_t n = table[0].size();
+    for(size_t i = 1, b = 1; b < n; ++i, b <<= 1){
+      table.push_back(table[0]);
+      for(size_t j = 0; j < n; j += b << 2){
+        size_t t = min(j + (b << 1), n), r = min(t + (b << 1), n);
+        for(size_t k = 0; t >= j+2+k; ++k) table[i][t-2-k] = table[0][t-2-k] + table[i][t-1-k];
         if(n <= t) break;
-        table.at(i).at(t) = v.at(t);
-        int r = min(t+shift, n);
-        for(int k = t+1; k < r; k++) table.at(i).at(k) = f(table.at(i).at(k-1), v.at(k));
+        for(size_t k = t+1; k < r; k++) table[i][k] = table[i][k - 1] + table[0][k];
       }
     }
-    lookup.assign(1 << b, 0);
-    for(int i = 2; i < (1 << b); i++){
-      lookup.at(i) = lookup.at(i>>1) + 1;
-    }
+    lookup.assign(1 << table.size(), 0);
+    for(size_t i = 2; i < lookup.size(); i++) lookup[i] = lookup[i >> 1] + 1;
   }
+  template<class Container>
+  DisjointSparseTable(const Container &c): DisjointSparseTable(begin(c), end(c)) {}
 
-  T query(int l, int r) const {
-    if(l >= --r) return table.at(0).at(l);
+
+  SemiGroup query(int l, int r) const {
+    if(l >= r) return SemiGroup{};
+    if(l >= --r) return table[0][l];
     int p = lookup.at(l ^ r);
-    return f(table.at(p).at(l), table.at(p).at(r));
+    return table[p][l] + table[p][r];
   }
 };
