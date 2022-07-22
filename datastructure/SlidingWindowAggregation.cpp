3d8e3fdf6f72848ff161940dbf8c1deab5051b09
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-07-22 12:47:24 +0900

    update

--- datastructure/SlidingWindowAggregation.cpp
+++ datastructure/SlidingWindowAggregation.cpp
@@ -1,43 +1,33 @@
-template <class T>
+template <class SemiGroup>
 struct SlidingWindowAggregation{
-  struct node{
-    T val, sum;
-    node(const T &val, const T &sum) : val(val), sum(sum){}
-  };
-  using OP = function<T(T,T)>;
-  OP op;
-  stack<node> front_stack, back_stack;
+  stack<pair<SemiGroup, SemiGroup>> front_stack, back_stack;
 
-  SlidingWindowAggregation(const OP &op): op(op), front_stack(), back_stack(){}
+  SlidingWindowAggregation(): front_stack(), back_stack(){}
 
   bool empty() const { return front_stack.empty() && back_stack.empty(); }
 
   size_t size() const { return front_stack.size() + back_stack.size(); }
 
-  T fold_all() const{
-    assert(!empty());
-    if(front_stack.empty()) return back_stack.top().sum;
-    if(back_stack.empty()) return front_stack.top().sum;
-    return op(front_stack.top().sum, back_stack.top().sum);
+  SemiGroup fold_all() const{
+    if(this->empty()) return SemiGroup{};
+
+    if(front_stack.empty()) return back_stack.top().second;
+    if(back_stack.empty()) return front_stack.top().second;
+    return front_stack.top().second + back_stack.top().second;
   }
 
-  void push(const T &x) {
-    if(back_stack.empty()){
-      back_stack.emplace(x,x);
-    }else{
-      T s{op(back_stack.top().sum, x)};
-      back_stack.emplace(x,s);
-    }
+  void push(const SemiGroup &x) {
+    if(back_stack.empty()) back_stack.emplace(x, x);
+    else back_stack.emplace(x, back_stack.top().second + x);
   }
 
   bool pop() {
-    if(empty()) return false;
+    if(this->empty()) return false;
     if(front_stack.empty()){
-      front_stack.emplace(back_stack.top().val,back_stack.top().val);
+      front_stack.emplace(back_stack.top().first, back_stack.top().first);
       back_stack.pop();
       while(!back_stack.empty()){
-        T s{op(back_stack.top().val, front_stack.top().sum)};
-        front_stack.emplace(back_stack.top().val, s);
+        front_stack.emplace(back_stack.top().first, back_stack.top().first + front_stack.top().second);
         back_stack.pop();
       }
     }
