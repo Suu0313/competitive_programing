1ca02899fa3eae2b79c155a330fab1383248d802
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-07-28 02:12:02 +0900

    update

--- datastructure/segtree/DualSegTree.cpp
+++ datastructure/segtree/DualSegTree.cpp
@@ -1,42 +1,55 @@
-template<typename M, typename H>
+template<class Monoid>
 struct DualSegTree{
   int n, height;
-  vector<M> lazy;
-  const H h;
-  const M id;
+  vector<Monoid> lazy;
 
-  DualSegTree(int n0, const H &h, const M &id): n(1), height(0), h(h), id(id){
-    while(n < n0){ n <<= 1; ++height; }
-    lazy.assign(n * 2, id);
+  DualSegTree() = default;
+  DualSegTree(int n): n(n), height(ilog2(n)), lazy(n * 2) {}
+  DualSegTree(int n, const Monoid &x): n(n), height(ilog2(n)), lazy(n * 2) {
+    for(int i = 0; i < n; ++i) lazy[i + n] = x;
+  }
+  template<typename Iiter>
+  DualSegTree(Iiter first, Iiter last): DualSegTree(int(distance(first, last))) {
+    for(int i = 0; i < n; ++i, ++first) lazy[i + n] = *first;
+  }
+  template<class Container>
+  DualSegTree(const Container &c): DualSegTree(begin(c), end(c)) {}
+
+
+  void update(int k, const Monoid &x){
+    if(x == Monoid{}) return;
+    resolve(k += n);
+    lazy[k] += x;
   }
 
-  void update(int l, int r, const M &x){
-    if(l >= r || x == id) return;
+  void update(int l, int r, const Monoid &x){
+    if(l >= r || x == Monoid{}) return;
     l += n; r += n;
-    for(int i = height; i>=1; --i) push(l >> i), push((r-1)>>i);
-    
-    for(; l < r; l>>=1, r>>=1){
-      if(l&1) lazy[l] = h(lazy[l], x), ++l;
-      if(r&1) --r, lazy[r] = h(lazy[r], x);
+    resolve(l); resolve(r - 1);
+
+    for(; l < r; l >>= 1, r >>= 1){
+      if(l & 1) lazy[l++] += x;
+      if(r & 1) lazy[--r] += x;
     }
   }
 
-  const M &operator[](int k){
-    k += n;
-    for(int i = height; i>=1; --i) push(k >> i);
+  const Monoid &operator[](int k){
+    resolve(k += n);
     return lazy[k];
   }
 
 private:
-  void push(int k){
-    if(lazy[k] == id) return;
-    lazy[2*k] = h(lazy[2*k], lazy[k]);
-    lazy[2*k + 1] = h(lazy[2*k + 1], lazy[k]);
-    lazy[k] = id;
+  int ilog2(int m){ return 31 - __builtin_clz(m); }
+
+  void resolve(int k){
+    Monoid id{};
+    for(int h = height + 2; h > 0; --h){
+      int i = k >> h;
+      if(lazy[i] != id){
+        lazy[i << 1] += lazy[i];
+        lazy[i << 1 | 1] += lazy[i];
+        lazy[i] = id;
+      }
+    }
   }
 };
-
-template<typename M, typename H>
-DualSegTree<M, H> get_dualsegtree(int n, const H &h, const M &id){
-  return {n, h, id};
-}
