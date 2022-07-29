3c58934f152bedbe5dee9206ed568fc55392e8ff
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-07-29 20:13:00 +0900

    create

---
+++ datastructure/algebraic/operator_monoid/RangeAffineRangeMinMaxSum.cpp
@@ -0,0 +1,12 @@
+template<typename T>
+struct RangeAffineRangeMinMaxSum{
+  using Monoid = MinMaxSumMonoid<T>;
+  using OperatorMonoid = CompositeMonoid<T>;
+
+  static void act(Monoid &m, const OperatorMonoid &f){
+    T x1 = f.a * m.mn + f.b;
+    T x2 = f.a * m.mx + f.b;
+    T sum = f.a * m.sum + f.b * m.len;
+    m = Monoid(min(x1, x2), max(x1, x2), sum, m.len);
+  }
+};
