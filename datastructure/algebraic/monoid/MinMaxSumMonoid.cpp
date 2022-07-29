2db370c9d2ed3877ec91f4c7ec58f5c1b1271b6b
Suu0313 <shuya_0313@docomo.ne.jp>
2022-07-29 20:13:00 +0900

    create

---
+++ datastructure/algebraic/monoid/MinMaxSumMonoid.cpp
@@ -0,0 +1,25 @@
+template<typename T>
+struct MinMaxSumMonoid{
+  T mn = numeric_limits<T>::min(), mx = numeric_limits<T>::max(), sum = 0;
+  size_t len = 0;
+  MinMaxSumMonoid() = default;
+  MinMaxSumMonoid(const MinMaxSumMonoid&) = default;
+  MinMaxSumMonoid(MinMaxSumMonoid&&) = default;
+
+  MinMaxSumMonoid(const T &x, size_t l = 1): mn(x), mx(x), sum(x * l), len(l) {}
+  MinMaxSumMonoid(const T &mn, const T &mx, const T &sum, size_t len): mn(mn), mx(mx), sum(sum), len(len) {}
+
+  MinMaxSumMonoid &operator=(const MinMaxSumMonoid&) = default;
+  MinMaxSumMonoid &operator=(MinMaxSumMonoid&&) = default;
+
+  MinMaxSumMonoid &operator+=(const MinMaxSumMonoid &m){
+    mn = min(mn, m.mn);
+    mx = max(mx, m.mx);
+    sum += m.sum;
+    len += m.len;
+    return (*this);
+  }
+  MinMaxSumMonoid operator+(const MinMaxSumMonoid &m) const {
+    return MinMaxSumMonoid(*this) += m;
+  }
+};
