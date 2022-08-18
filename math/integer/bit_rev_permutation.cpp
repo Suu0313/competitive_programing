59c7b572c3c1a13338a163aab83bc89f2dc567af
Suu0313 <shuya_0313@docomo.ne.jp>
2022-08-19 03:37:38 +0900

    create

---
+++ math/integer/bit_rev_permutation.cpp
@@ -0,0 +1,11 @@
+vector<int> bit_rev_perm(int k){
+  int n = 1 << k;
+  vector<int> a(n);
+  for(int i = 0, l = 1; i < k; ++i, l <<= 1){
+    n >>= 1;
+    for(int j = 0; j < l; ++j){
+      a[l + j] = a[j] + n;
+    }
+  }
+  return a;
+}
