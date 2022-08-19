e129559d52b306b5caf38a9d5d0313383c323b75
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-08-20 08:54:51 +0900

    update

--- math/fps/ntt-original.cpp
+++ math/fps/ntt-original.cpp
@@ -1,18 +1,12 @@
-namespace NTT{
+template<class modint>
+struct NTT{
+  static int ps, cnt;
+  static modint root, zeta;
+  static modint omegas[30], invomegas[30];
 
-  vector<int> bit_rev_perm(int k){
-    int n = 1 << k;
-    vector<int> a(n);
-    for(int i = 0, l = 1; i < k; ++i, l <<= 1){
-      n >>= 1;
-      for(int j = 0; j < l; ++j){
-        a[l + j] = a[j] + n;
-      }
-    }
-    return a;
-  }
+  static int bsf(unsigned int n) { return __builtin_ctz(n); }
 
-  long long pow_mod(long long x, long long n, int m) {
+  static long long pow_mod(long long x, long long n, int m) {
     if (m == 1) return 0;
     unsigned int _m = (unsigned int)(m);
     unsigned long long r = 1;
@@ -20,59 +14,83 @@ namespace NTT{
     if (x < 0) x += m;
     while (n) {
         if (n & 1) r = (r * x) % _m;
-        x = (x * x) % _m;
-        n >>= 1;
+        x = (x * x) % _m; n >>= 1;
     }
     return r;
   }
 
-  int primitive_root(int m){
-      if (m == 2) return 1;
-      if (m == 167772161) return 3;
-      if (m == 469762049) return 3;
-      if (m == 754974721) return 11;
-      if (m == 998244353) return 3;
-      int divs[20] = {};
-      divs[0] = 2;
-      int cnt = 1;
-      int x = (m-1) / 2;
-      while(x%2 == 0) x >>= 1;
-      for(int i=3; (long long)(i)*i <= x; i+=2){
-        if(x%i == 0){
-          divs[cnt++] = i;
-          while (x % i == 0) x /= i;
-        }
-      }
-      if(x > 1) divs[cnt++] = x;
-
-      for (int g = 2;; g++) {
-          bool ok = true;
-          for (int i = 0; i < cnt; i++) {
-              if (pow_mod(g, (m - 1) / divs[i], m) == 1) {
-                  ok = false;
-                  break;
-              }
-          }
-          if (ok) return g;
+  static int primitive_root(int m){
+    if (m == 2) return 1;
+    if (m == 167772161) return 3;
+    if (m == 469762049) return 3;
+    if (m == 754974721) return 11;
+    if (m == 998244353) return 3;
+    int divs[20] = {};
+    divs[0] = 2;
+    int cnt = 1;
+    int x = (m-1) / 2;
+    while(x%2 == 0) x >>= 1;
+    for(int i=3; (long long)(i)*i <= x; i+=2){
+      if(x%i == 0){
+        divs[cnt++] = i;
+        while (x % i == 0) x /= i;
       }
     }
+    if(x > 1) divs[cnt++] = x;
 
-  int bsf(unsigned int n) { return __builtin_ctz(n); }
+    for (int g = 2;; g++) {
+        bool ok = true;
+        for (int i = 0; i < cnt; i++) {
+            if (pow_mod(g, (m - 1) / divs[i], m) == 1) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) return g;
+    }
+  }
 
-  template<class modint>
-  void ntt(vector<modint> &a, modint zeta, int k, const vector<int> &rev){
+  static void precalculate(){
+    static bool precalculated = false;
+    if(precalculated) return;
+    ++acnt;
+    precalculated = true;
+    int mod = modint::get_mod();
+    cnt = bsf(mod - 1);
+    ps =  (mod - 1) >> cnt;
+    root = primitive_root(mod);
+    zeta = root.pow(ps);
+    omegas[cnt] = zeta;
+    invomegas[cnt] = zeta.inverse();
+    for(int i = cnt - 1; i >= 0; --i){
+      omegas[i] = omegas[i + 1] * omegas[i + 1];
+      invomegas[i] = invomegas[i + 1] * invomegas[i + 1];
+    }
+  }
+
+  static void ntt(vector<modint> &a, int k){
     int n = 1 << k;
     a.resize(n);
-    vector<modint> omegas(k);
-    omegas[k - 1] = zeta;
-    for(int i = k-2; i >= 0; --i) omegas[i] = omegas[i + 1] * omegas[i + 1];
-    for(int i = 0; i < n; ++i) if(i > rev[i]) swap(a[i], a[rev[i]]);
-
-    for(int i = 0; i < k; ++i){
-    modint omega = 1, omega_m = omegas[i];
+    for(int i = k - 1; i >= 0; --i){
+    modint omega = 1, omega_m = omegas[i + 1];
       for(int j = 0, m = (1 << i); j < m; ++j){
         for(int k = j; k < n; k += m * 2){
-          modint u = a[k], t = omega * a[k + m];
+          mint u = a[k], t = a[k + m];
+          a[k] = u + t; a[k + m] = (u - t) * omega;
+        }
+        omega *= omega_m;
+      }
+    }
+  }
+
+  static void invntt(vector<mint> &a, int k){
+    int n = 1 << k;
+    a.resize(n);
+    for(int i = 0; i < k; ++i){
+    mint omega = 1, omega_m = invomegas[i + 1];
+      for(int j = 0, m = (1 << i); j < m; ++j){
+        for(int k = j; k < n; k += m * 2){
+          mint u = a[k], t = omega * a[k + m];
           a[k] = u + t; a[k + m] = u - t;
         }
         omega *= omega_m;
@@ -80,8 +98,7 @@ namespace NTT{
     }
   }
 
-  template<class modint>
-  vector<modint> multiply(vector<modint> a, vector<modint> b){
+  static vector<modint> multiply(vector<modint> a, vector<modint> b){
     int n = int(a.size()), m = int(b.size());
     if(min(n, m) <= 60){
       vector<modint> ans(n + m - 1);
@@ -90,21 +107,23 @@ namespace NTT{
           ans[i + j] += a[i] * b[j];
       return ans;
     }
-
+    precalculate();
     int k = 0;
     while((n + m - 1) > (1 << k)) ++k;
-    int mod = modint::get_mod(), ps = mod - 1, cnt = bsf(ps);
-    ps >>= cnt;
-    assert(cnt >= k);
-    modint root = primitive_root(mod), zeta = root.pow(ps);
-    for(int i = 0; i < cnt - k; ++i) zeta *= zeta;
-    vector<int> rev = bit_rev_perm(k);
-    ntt(a, zeta, k, rev); ntt(b, zeta, k, rev);
+    ntt(a, k); ntt(b, k);
     for(int i = 0; i < (1 << k); ++i) a[i] *= b[i];
-    ntt(a, zeta.inverse(), k, rev);
+    invntt(a, k);
     a.resize(n + m - 1);
     modint inv2 = modint(1 << k).inverse();
     for(auto&&e : a) e *= inv2;
     return a;
   }
-}
+};
+
+template<class modint> int NTT<modint>::ps = 0;
+template<class modint> int NTT<modint>::cnt = 0;
+template<class modint> modint NTT<modint>::root = 0;
+template<class modint> modint NTT<modint>::zeta = 0;
+template<class modint> modint NTT<modint>::omegas[] = {};
+template<class modint> modint NTT<modint>::invomegas[] = {};
+// TODO: 4-radix
