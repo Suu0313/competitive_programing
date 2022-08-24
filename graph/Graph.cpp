a45bf8ffb7b4ff99e448ca9b78b28c6f7b45fe02
Suu0313 <shuya_0313@docomo.ne.jp>
2022-08-24 23:41:11 +0900

    update

--- graph/Graph.cpp
+++ graph/Graph.cpp
@@ -1,32 +1,27 @@
 template<typename T = int>
 struct Edge{
   int src, to; T cost; int idx;
-  Edge(){}
+  Edge() = default;
   Edge(int src, int to, T cost = -1, int idx = -1): src(src), to(to), cost(cost), idx(idx) {}
   operator int() const { return to; }
 };
 template<typename T> using Edges = vector<Edge<T>>;
 
-template<typename T = bool>
-struct Graph {
-  vector<vector<Edge<T>>> g;
+template<typename T = int>
+struct Graph : vector<vector<Edge<T>>> {
+  using base_t = vector<vector<Edge<T>>>;
+  using base_t::base_t;
   size_t es;
-  Graph() {};
-  Graph(size_t n): g(n), es(0) {}
-  size_t size() const { return g.size(); }
-  int deg(int v) const { return int(g[v].size()); }
+  Graph(size_t n): base_t(n), es(0) {}
 
-  vector<Edge<T>> &operator[](int k) { return g.at(k); }
-  const vector<Edge<T>> &operator[](int k) const { return g.at(k); }
-  vector<Edge<T>> &at(int k) { return g.at(k); }
-  const vector<Edge<T>> &at(int k) const { return g.at(k); }
+  int deg(int v) const { return int(base_t::operator[](v).size()); }
 
   void add_edge(int src, int to, T cost = 1){
-    g[src].emplace_back(src, to, cost, es);
-    g[to].emplace_back(to, src, cost, es++);
+    base_t::operator[](src).emplace_back(src, to, cost, es);
+    base_t::operator[](to).emplace_back(to, src, cost, es++);
   }
   void add_directed_edge(int src, int to, T cost = 1){
-    g[src].emplace_back(src, to, cost, es++);
+    base_t::operator[](src).emplace_back(src, to, cost, es++);
   }
 
   void read(int m, int base = 1, bool weighted = false, bool directed = false, const T &id = 1){
@@ -40,10 +35,10 @@ struct Graph {
   }
   
   vector<int> topological_order() const {
-    int n = int(g.size());
+    int n = int(this->size());
     vector<int> vs, ind(n); vs.reserve(n);
-    for(int v = 0; v < n; ++v){
-      for(const auto&e : g[v]) ++ind[e];
+    for(const auto&edges : (*this)){
+      for(const auto&edge : edges) ++ind[edge.to];
     }
     queue<int> qu;
     for(int v = 0; v < n; ++v){
@@ -52,24 +47,38 @@ struct Graph {
     while(!qu.empty()){
       int v = qu.front(); qu.pop();
       vs.push_back(v);
-      for(auto e : g[v]){
-        if(--ind[e] == 0) qu.push(e);
+      for(const auto &e :  base_t::operator[](v)){
+        if(--ind[e.to] == 0) qu.push(e.to);
       }
     }
     return vs;
   }
 
   Graph<T> get_directed_tree(int root = 0) const {
-    Graph<T> tree(g.size());
+    Graph<T> tree(this->size());
     queue<pair<int, int>> qu;
     for(qu.emplace(root, -1); !qu.empty(); qu.pop()){
       auto[v, p] = qu.front();
-      for(const auto &e : g[v]){
+      for(const auto &e : base_t::operator[](v)){
         if(e.to == p) continue;
         tree.add_directed_edge(v, e.to, e.cost);
         qu.emplace(e.to, v);
       }
     }
     return tree;
+  }
+
+  vector<T> bfs_dist(int root) const {
+    vector<T> dist(this->size());
+    queue<pair<int, int>> qu;
+    for(qu.emplace(root, -1); !qu.empty(); qu.pop()){
+      auto[v, p] = qu.front();
+      for(const auto &e : base_t::operator[](v)){
+        if(e.to == p) continue;
+        dist[e.to] = dist[v] + e.cost;
+        qu.emplace(e.to, v);
+      }
+    }
+    return dist;
   }
 };
