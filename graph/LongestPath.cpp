2ba81329ce9fb2312bdffe61bc0d0e9d7710eb16
Suu0313 <shuya_0313@docomo.ne.jp>
2022-08-31 23:35:07 +0900

    mv

--- graph/LongestPath.cpp
+++
@@ -1,9 +0,0 @@
-template<typename T>
-int LestP(vector<vector<T>> &G, int i, vector<int> &dp){
-  if(dp.at(i) >= 0) return dp.at(i);
-  int res = 0;
-  for(auto e : G.at(i)){
-    res = max(res,LestP(G,e,dp)+1);
-  }
-  return dp.at(i) = res;
-}
