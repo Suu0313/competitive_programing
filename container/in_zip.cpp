655c70444edd1ea45fbf75d4945cb3499441f449
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-09-08 03:27:08 +0900

    add copy

--- container/in_zip.cpp
+++ container/in_zip.cpp
@@ -2,7 +2,7 @@ template<class Container> auto myref(Container &c, size_t i){ return ref(c[i]); 
 template<class Container> auto myref(Container &&c, size_t i){ return c[i]; }
 
 template<class... Cs>
-auto inzip(Cs&&... cs){
+auto in_zip_ref(Cs&&... cs){
   vector<tuple<conditional_t<is_lvalue_reference_v<Cs>, add_lvalue_reference_t<typename remove_reference_t<Cs>::value_type>, typename remove_reference_t<Cs>::value_type>...>> v;
   size_t n = min({size(cs)...}); v.reserve(n);
   for(size_t i = 0; i < n; i++) v.emplace_back(myref(std::forward<Cs>(cs), i)...);
@@ -10,7 +10,22 @@ auto inzip(Cs&&... cs){
 }
 
 template<class... Cs>
-auto enumerate(Cs&&... cs){
+auto enumerate_ref(Cs&&... cs){
   auto iota_impl = [&]{ vector<int> idx(min({size(cs)...})); iota(begin(idx), end(idx), 0); return idx; };
   return inzip(iota_impl(), cs...);
+}
+
+template<class... Cs>
+auto in_zip(Cs&&... cs){
+  vector<tuple<typename remove_reference_t<Cs>::value_type ...>> v;
+  size_t n = min({size(cs)...}); v.reserve(n);
+  for(size_t i = 0; i < n; i++) v.emplace_back(cs[i]...);
+  return v;
+}
+
+template<class... Cs>
+auto enumerate(Cs&&... cs){
+  vector<int> idx(min({size(cs)...}));
+  iota(begin(idx), end(idx), 0);
+  return in_zip(idx, cs...);
 }
