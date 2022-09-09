e1b1c3e16fa6733fa150d9306f6e6a76b8b56ad8
Suu0313 <shuya_0313@docomo.ne.jp>
2022-09-09 16:07:06 +0900

    rm zip

--- template.cpp
+++ template.cpp
@@ -152,22 +152,6 @@ vector<T> get_pow_vector(int n, T p){
   for(int i = 0; i < n; ++i) res[i + 1] = res[i] * p;
   return res;
 }
-template<class Container> auto myref(Container &c, size_t i){ return ref(c[i]); }
-template<class Container> auto myref(Container &&c, size_t i){ return c[i]; }
-
-template<class... Cs>
-auto in_zip(Cs&&... cs){
-  vector<tuple<conditional_t<is_lvalue_reference_v<Cs>, add_lvalue_reference_t<typename remove_reference_t<Cs>::value_type>, typename remove_reference_t<Cs>::value_type>...>> v;
-  size_t n = min({size(cs)...}); v.reserve(n);
-  for(size_t i = 0; i < n; i++) v.emplace_back(myref(std::forward<Cs>(cs), i)...);
-  return v;
-}
-
-template<class... Cs>
-auto enumerate(Cs&&... cs){
-  auto iota_impl = [&]{ vector<int> idx(min({size(cs)...})); iota(begin(idx), end(idx), 0); return idx; };
-  return in_zip(iota_impl(), cs...);
-}
 
 bool cYN(bool fl=true,bool fl2=false){cout << (fl?"Yes":"No") << '\n'; if(fl2){ exit(0); } return fl; }
 bool CYN(bool fl=true,bool fl2=false){cout << (fl?"YES":"NO") << '\n'; if(fl2){ exit(0); } return fl; }
