7ccb3e74be0e844fd4e509dfcf0d69891ab4264f
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2022-09-13 20:58:09 +0900

    create

--- datastructure/trie/Trie.cpp
+++ datastructure/trie/Trie.cpp
@@ -1,90 +1,52 @@
-template<int W = 10>
+template<typename T>
 struct Trie{
-  vector<array<int, W>> to;
-  // vector<size_t> sz, cnt;
+  Trie(): ptr(1), sz(1) {}
 
-  // Trie(): to(1, array<int, W>{0}), sz(1), cnt(1) {}
+  vector<map<T, size_t>> ptr;
+  vector<size_t> sz;
 
-  // Trie(int n): to(1, array<int, W>{0}), sz(1), cnt(1) {
-  //   to.reserve(n); sz.reserve(n); cnt.reserve(n);
-  // }
-
-  // size_t size() const { return sz[0]; }
-
-  // bool empty() const { return !sz[0]; }
-
-  // size_t count(const vector<int> &v) const {
-  //   int pos = 0;
-  //   for(int x : v){
-  //     if(to[pos][x] == 0) return 0;
-  //     pos = to[pos][x];
-  //   }
-  //   return cnt[pos];
-  // }
-
-  Trie(): to(1, array<int, W>{0}) {}
-
-  int insert(const vector<int> &v){
-    int pos = 0; // ++sz[pos];
-    for(int x : v){
-      if(to[pos][x] == 0) to[pos][x] = add_node();
-      // ++sz[pos = to[pos][x]];
-      pos = to[pos][x];
-    }
-    // ++cnt[pos];
-    return pos;
-  }
-
-  // int erase(const vector<int> &v){
-  //   int pos = 0;
-  //   for(int x : v){
-  //     if(to[pos][x] == 0 || !sz[to[pos][x]]) return -1;
-  //     pos = to[pos][x];
-  //   }
-  //   if(!cnt[pos]) return -1;
-
-  //   pos = 0; --sz[pos];
-  //   for(int x : v){
-  //     --sz[pos = to[pos][x]];
-  //   }
-  //   --cnt[pos];
-  //   return pos;
-  // }
-
-  vector<int> ord;
-  void build(){
-    ord.resize(to.size());
-    stack<int> st; st.push(0);
-    int t = 0;
-
-    while(!st.empty()){
-      int pos = st.top(); st.pop();
-      ord[pos] = t++;
-      for(int x = W-1; x >= 0; --x){
-        if(to[pos][x] != 0) st.push(to[pos][x]);
+  template<class Iter>
+  int add(Iter first, Iter last){
+    int i = 0; ++sz[0];
+    for(; first != last; ++first){
+      T c = *first;
+      if(auto it = ptr[i].find(c); it == ptr[i].end()){
+        ptr[i][c] = sz.size();
+        sz.push_back(0);
+        ptr.push_back({});
       }
+      sz[i = ptr[i][c]] += 1;
     }
-  }
-
-  int get_ord(const vector<int> &v) const {
-    int pos = 0;
-    for(int x : v){
-      if(to[pos][x] == 0) return -1;
-      pos = to[pos][x];
-    }
-    return ord[pos];
-  }
-
-  int next(int pos, int x) const {
-    return to[pos][x];
-  }
-
-private:
-  size_t add_node(){
-    size_t i = to.size();
-    to.push_back({0, 0});
-    //sz.push_back(0);
-    //cnt.push_back(0);
     return i;
   }
+
+  template<class Container>
+  int add(const Container &c){  return add(begin(c), end(c)); }
+
+  template<class Iter>
+  int cnt(Iter first, Iter last) const {
+    int i = 0;
+    for(; first != last; ++first){
+      T c = *first;
+      if(const auto it = ptr[i].find(c); it == ptr[i].end()){
+        return 0;
+      }
+      i = ptr[i][c];
+    }
+    return sz[i];
+  }
+
+  template<class Container>
+  size_t cnt(const Container &c) const { return cnt(begin(c), end(c)); }
+
+  vector<vector<int>> get_graph() const {
+    int n = int(sz.size());
+    vector<vector<int>> g(n);
+    for(int i = 0; i < n; ++i){
+      for(const auto[c, to] : ptr[i]) g[i].push_back(to);
+    }
+    return g;
+  }
+
+  size_t size() const { return sz.size(); }
 };
