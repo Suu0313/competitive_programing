1105385cc69c8372d15cef62cf21974a6bb838fa
Suu0313 <62999703+Suu0313@users.noreply.github.com>
2023-01-17 19:47:30 +0900

    bug fix

--- datastructure/algebraic/monoid/Monoid.cpp
+++ datastructure/algebraic/monoid/Monoid.cpp
@@ -15,7 +15,7 @@ struct Monoid{
     return (*this);
   }
 
-  Monoid operator+(const Monoid &m){ return Monoid(*this) += m; }
+  Monoid operator+(const Monoid &m) const { return Monoid(*this) += m; }
 
   bool operator==(const Monoid &m) const { return x == m.x; }
   bool operator!=(const Monoid &m) const { return !((*this) == m); }
