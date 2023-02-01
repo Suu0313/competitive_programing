04cf2e5ff13fa283e8ee79234481de660a5e5731
Suu0313 <shuya_0313@docomo.ne.jp>
2023-02-02 08:15:01 +0900

    rn

--- string/manacher.cpp
+++ string/manacher.cpp
@@ -1,19 +1,19 @@
 template<class Container>
-vector<int> Manacher(const Container &s){
+vector<int> manacher(const Container &s){
   int n = int(s.size());
   Container t(n*2 + 1, s[0]);
   for(int i = 0; i < n; ++i) t[i*2 + 1] = s[i];
   n = int(t.size());
-  vector<int> rad(n);
+  vector<int> dia(n);
   for(int i = 0, j = 0; i < n; ){
     while(i - j >= 0 && i + j < n && t[i - j] == t[i + j]) ++j;
-    rad[i] = j;
+    dia[i] = j;
     int k = 1;
-    for( ;i - k >= 0 && i + k < n && k + rad[i - k] < j; ++k){
-      rad[i + k] = rad[i - k];
+    for( ;i - k >= 0 && i + k < n && k + dia[i - k] < j; ++k){
+      dia[i + k] = dia[i - k];
     }
     i += k; j -= k;
   }
-  for(auto&e : rad) --e;
-  return rad;
+  for(auto&e : dia) --e;
+  return dia;
 }
