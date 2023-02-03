2373008e9684961df36edb86e1a869625973c5b0
Suu0313 <shuya_0313@docomo.ne.jp>
2023-02-03 16:12:02 +0900

    2変数を一個にまとめて map を unordered_map に変更

--- datastructure/flow/ProjectSelectionProblem.cpp
+++ datastructure/flow/ProjectSelectionProblem.cpp
@@ -3,11 +3,15 @@ struct ProjectSelectionProblem{
   int n, s, t;
   Dinic<T> dinic;
   vector<T> theta0, theta1;
-  map<pair<int, int>, array<T, 4>> phi;
+  unordered_map<int64_t, array<T, 4>> phi;
   T res = 0;
 
   ProjectSelectionProblem() = default;
   ProjectSelectionProblem(int n): n(n), s(n + 1), t(s + 1), dinic(t + 1), theta0(n), theta1(n) {}
+
+
+  int64_t f(int i, int j) const { return int64_t(i) * n + j; }
+  pair<int, int> f(int64_t x) const { return {x/n, x%n}; }
 
   void add_theta(int i, bool b, const T &x){
     if(b) theta1[i] += x;
@@ -16,13 +20,14 @@ struct ProjectSelectionProblem{
 
   void add_phi(int i, int j, bool bi, bool bj, const T &x){
     if(i > j) swap(i, j), swap(bi, bj);
-    if(phi.find({i, j}) == phi.end()) phi[{i, j}] = {0, 0, 0, 0};
-    phi[{i, j}][int(bi) << 1 | bj] += x;
+    int64_t ij = f(i, j);
+    if(phi.find(ij) == phi.end()) phi[ij] = {0, 0, 0, 0};
+    phi[ij][int(bi) << 1 | bj] += x;
   }
 
   T solve(){
     for(auto&[p, v] : phi){
-      auto[i, j] = p;
+      auto[i, j] = f(p);
       res += v[0];
       add_theta(i, 1, v[2] - v[0]);
       add_theta(j, 1, v[3] - v[2]);
@@ -47,4 +52,3 @@ struct ProjectSelectionProblem{
     return res + dinic.flow(s, t);
   }
 };
-
