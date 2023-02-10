95388179e50457f78edf6f048f5947aa3ab52e7b
Suu0313 <shuya_0313@docomo.ne.jp>
2023-02-10 10:58:16 +0900

    sep templates

---
+++ template/template_constants.cpp
@@ -0,0 +1,9 @@
+constexpr double EPS = 1e-10;
+constexpr double PI  = 3.141592653589793238462643383279;
+constexpr int INF = numeric_limits<int>::max()/2;
+constexpr long long LINF = numeric_limits<long long>::max()/3;
+constexpr long long MOD1 = 1e9+7;
+constexpr long long MOD2 = 998244353;
+constexpr long long TEN(int n) { return n? 10*TEN(n-1) : 1; }
+
+constexpr int dx[] = {1,0,-1,0,1,1,-1,-1}, dy[] = {0,1,0,-1,1,-1,1,-1};
---
+++ template/template_integers.cpp
@@ -0,0 +1,11 @@
+#include "..\math\integer\ceil.cpp"
+
+#include "..\math\integer\floor.cpp"
+
+#include "..\math\integer\floor_kth_root.cpp"
+
+#include "..\math\integer\isqrt.cpp"
+
+#include "..\math\integer\ipow.cpp"
+
+#include "..\math\integer\popcount.cpp"
